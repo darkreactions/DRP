faa681f932053127648133e86c4f400015c4268b
Casey Falk <caseyfalk94@gmail.com>
2014-04-21 16:27:46 -0400

    Pushing recommendation scripts that we worked on last week.

--- DRP/research/seed_rec.py
+++ DRP/research/seed_rec.py
@@ -1,5 +1,8 @@
 import load_cg, parse_rxn, uuid
 import rebuildCDT
+import metrics
+
+sim = metrics.Euclidean().sim
 
 #Variable Setup.
 CG = load_cg.load_cg()
@@ -11,6 +14,10 @@ reactant_fields = {
 	"reactant_4": ("quantity_4", "unit_4"),
 	"reactant_5": ("quantity_5", "unit_5"),
 }
+
+pH_range = [1,3,5]
+number_of_amine_mole_steps = 10
+steps_per_amine = len(pH_range)*number_of_amine_mole_steps 
 
 def get_reactants_indices(reaction):
 	indices = dict()
@@ -71,8 +78,45 @@ def row_generator(reaction, indices, amine_moles, amine_list):
 			for pH in ph_range:
 				yield ["--", metal_1, metal_1_mass, "g", metal_2, metal_2_mass,
 					"g", amine, mass, "g", water, water_mass, "g", 
-					reaction["temp"], reaction["time"], reaction["pH"], "yes", "no", 4, 2, ""]
+					reaction["temp"], reaction["time"], pH, "yes", "no", 4, 2, ""]
 
+def get_candidates(results, idx, raw_rows):
+	candidates = []
+	for i in range(steps_per_amine):
+		if i + idx in results:
+			candidates.append( (conf, raw_rows[i+idx]))
+	if len(candidates) == 0:
+		return None, 0.0
+	best_conf = max(candidates, key=lambda x: x[0])
+	candidates = filter(lambda x: x == best_conf, candidates)
+	candidates = [c[1] for c in candidates]
+
+	avg_mass = 0.0
+	avg_ph = 0.0
+	for r in candidates:
+		avg_mass += r[8]
+		avg_ph += r[15]
+	
+	avg_mass = avg_mass / float(len(candidates))
+	avg_ph = avg_ph / float(len(candidates))
+
+	smallest_dist = 10000000000000000000
+	best_row = None
+
+	for row in candidates:
+		dist = (avg_mass-row[8])**2 + (avg_ph - r[15])**2
+		if dist < smallest_dist:
+			smallest_dist = dist
+			best_row = row
+
+	assert(best_row is not None)
+
+	return best_row, best_conf
+
+def sim(a, b):
+	return 1.0	
+	
+		 
 def generate_grid(reaction, amine_list):
 	#Variable Setup.
 	indices = get_reactions_indices(reaction)
@@ -81,8 +125,11 @@ def generate_grid(reaction, amine_list):
 	fileprefix = str(uuid.uuid4())
 	ml_convert = json.load(open("mlConvert.json"))
 	hdrs = ",".join(rebuildCDT.headers)
+
 	row_gen = row_generator(reaction, indices, amine_moles, amine_list)
-	rows =[ ",".join([str(c).replace(",","c") for c in parse_rxn.parse_rxn(row, CG, ml_convert)])) for row in row_gen] 
+	raw_rows = [row for row in row_gen]
+	rows =[ ",".join([str(c).replace(",","c") for c in parse_rxn.parse_rxn(row, CG, ml_convert)])) for row in raw_rows] 
+
 	with open(prefix + fileprefix + ".csv", "w") as outfile:
 		for row in rows:
 			outfile.write(row+"\n")
@@ -94,17 +141,31 @@ def generate_grid(reaction, amine_list):
 	result = subprocess.check_output(cmd, shell=True)
 
 	weka_results_file = "/home/drp/blablabla..."
-	results = []
+	results = dict()
 	with open(prefix + weka_results_file, "r") as weka_results:
 		for i in xrange(5):
 			weka_results.next()
 
-		for row in weka_results:
+		for i, row in enumerate(weka_results):
 			if "4" in row and "+" not in row:
 				conf = float(row.split()[-1])
-				idx = int(row.split()[0])
-				results.append( (idx, conf))
+				results[i] =  conf
+	
 
+	amines_results = [] 
+	for i, amine in enumerate(amine_list):
+		best_candidate, best_conf = get_candidates(results,i*steps_per_amine, raw_rows)
+		if best_candidate:
+			amines_results.append( (best_conf, sim(amine, reaction[indices["amine"]]), best_candidate) )
+
+
+	amines_results.sort(key=lambda x: x[0]*x[1], reverse=True)
+	return amines_results
+
+	
+
+	
+		
 
 
 
