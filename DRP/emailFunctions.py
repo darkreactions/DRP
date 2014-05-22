34fabd3d5e09a71e8d7155d0326cd18b2a1bd682
Casey Falk <caseyfalk94@gmail.com>
2014-05-22 17:31:18 -0400

    Automating compound-calc process.

--- DRP/emailFunctions.py
+++ DRP/emailFunctions.py
@@ -3,11 +3,13 @@ from django.core.mail import send_mail
 from DRP.retrievalFunctions import *
 from DRP.errorReporting import print_error
 
-######################  Email Functions  ###########################
-def alert_about_new_lab(lab_group):
-  email_body = "A new Lab Group has been registered:"
-  email_body += "\n{}\n{}\n{}".format(lab_group.lab_title, lab_group.lab_address, lab_group.lab_email) 
-  send_mail("DRP: New Lab Group Registered", email_body, settings.EMAIL_HOST_USER, [settings.EMAIL_HOST_USER], fail_silently=False)
+######################  Email Functions  ########################### 
+def email_admins(subject, message):
+  try:
+    send_mail("DRP: {}".format(subject), message, settings.EMAIL_HOST_USER, [settings.EMAIL_HOST_USER], fail_silently=False)
+  except Exception as e:
+    print_error("email_admins failed: {}\n".format(e))
+
 
 def email_user(user, subject, message):
   try:
@@ -15,6 +17,7 @@ def email_user(user, subject, message):
               [user.email], fail_silently=False)
   except:
     print_error("email_user failed: {}\n".format(user.id))
+
 
 def email_lab(lab, subject, message, include_members=False):
   try:
@@ -29,3 +32,8 @@ def email_lab(lab, subject, message, include_members=False):
     for user in users:
       email_user(user, "(Lab) {}".format(subject), message)
 
+
+######################  Email Wrappers  ########################### 
+def alert_about_new_lab(lab_group):
+  email_body = "A new lab group has been registered:\n{}\n{}\n{}".format(lab_group.lab_title, lab_group.lab_address, lab_group.lab_email) 
+  email_admins("New Lab Group Registered", email_body)
