8454e21cfa5941d03678efa853b58fb4f03244f5
Casey Falk <caseyfalk94@gmail.com>
2015-02-07 13:18:08 -0500

    Added more setup help and made setting.py even more flexible.

--- DRP/data_config.py
+++ DRP/data_config.py
@@ -13,38 +13,38 @@ strptime = datetime.datetime.strptime
 class ConfigManager(object):
   def __init__(self):
     from DRP.fileFunctions import file_exists
-
+    from DRP.settings import LICENSE_DIR, LIBRARY_DIR
 
     validate_config = True
 
-    #Database Setup Variables
+    # Database Setup Variables
     self.num_reactants = 5 #The number of reactants supported.
     self.reactants_required = 2 #The number of reactants required.
     self.fields_per_reactant = 3 #Each reactant has a name, a quantity, and a unit.
-    #Note: if num_reactants changes, database must be migrated.
+    # Note: if num_reactants changes, database must be migrated.
 
-    #Database Page View Variables
+    # Database Page View Variables
     self.current_page_radius = 3 #The number of pages to show "around" the current page.
     self.data_per_page = 15 #The number of reactions to show per page.
 
-    #Data Upload Variables
+    # Data Upload Variables
     self.blacklist = {"x", "-1", -1, "z", "?", "", " "} #Implies absence of data.
     self.unknown_label = "?" #The label that blacklisted values will inherit.
     self.not_required_label = "" #The label that auto-added values will inherit if empty.
 
-    #Licensing/legal Setup
+    # Licensing/legal Setup
     self.raw_license_date = "2014-01-20 01:00:00.000000"
-    self.current_license_date = strptime(self.raw_license_date, "%Y-%m-%d %X.%f")
-    self.current_license_file = "/licenses/01_20_14.pdf" #Assumed to be in STATIC directory.
+    self.license_date = strptime(self.raw_license_date, "%Y-%m-%d %X.%f")
+    self.license_file = LICENSE_DIR + "/01_20_14.pdf"
 
-    #General Variable Setup
-    self.jchem_path = "~/ChemAxon/JChem/bin"
-    self.weka_path = "~/weka/weka.jar"
+    # Path Setup
+    self.jchem_path = LIBRARY_DIR+"/ChemAxon/JChem/bin"
+    self.weka_path = LIBRARY_DIR+"/weka/weka.jar"
 
 
     if validate_config:
       # Make sure all the hard-coded paths are valid.
-      for path in [self.jchem_path, self.weka_path]:
+      for path in [self.jchem_path, self.weka_path, self.license_file]:
         if not file_exists(path):
           raise Exception("Path `{}` not found!".format(path))
 
