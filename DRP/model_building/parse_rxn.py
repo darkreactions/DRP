d48d70ca0f23bc414a5b2be6723fc25a06e90b54
Casey Falk <cfalk@drp.(none)>
2015-02-11 21:19:06 -0500

    Fixed a few bugs in data-calculations generation.

--- DRP/model_building/parse_rxn.py
+++ DRP/model_building/parse_rxn.py
@@ -29,8 +29,7 @@ def parse_rxn(row, rxn_table, ml_convert):
     for i in range(5):
         cmpd = compound[i].lower()
         if cmpd == "water": continue
-        print cmpd in rxn_table
-        if cmpd == "" or rxn_table[cmpd]["type"] in ['pH']:
+        if not cmpd or rxn_table[cmpd]["type"] in ['pH']:
             compound[i] = 'x'
             mass[i] = '-1'
         elif "mL" in unit[i]:
