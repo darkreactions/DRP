b09bdb20a4b84c0392f76cc7ec51ef0e90e7954d
Casey Falk <caseyfalk94@gmail.com>
2015-02-23 22:57:33 -0500

    Generalized Data calculation-gathering to work for Recommendations.

--- DRP/views/compound_guide.py
+++ DRP/views/compound_guide.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.http import require_http_methods
 
 from DRP.models import *
 
