944879fd5c076a13783d8bfc9ee60cd0bbe40e5c
Casey Falk <caseyfalk94@gmail.com>
2015-03-19 13:38:50 -0400

    Removed PW from settings file (not history). Removed results/CSV from repo.

--- DRP/research/casey/csv_output.py
+++ DRP/research/casey/csv_output.py
@@ -36,7 +36,9 @@ def split_and_write():
   from DRP.model_building.generate_models import research_data_filter
   from DRP.retrievalFunctions import get_valid_data
   from DRP.model_building.rxn_calculator import headers
+  from DRP.models import Recommendation
 
+  from django.db.models import Q
   import time
 
   num_splits = 10
@@ -46,6 +48,24 @@ def split_and_write():
   data = [headers] + list(research_data_filter(data))
   data = preprocessor(data)
 
+  print "Calculating train_supplement"
+  calculated = ~Q(calculations=None)
+  nonsense_recs = Recommendation.objects.filter(Q(nonsense=True), calculated)
+
+  clean_recs = []
+  for i, rec in enumerate(nonsense_recs):
+
+    try:
+      rec.get_calculations_list()
+      clean_recs.append(rec)
+    except Exception as e:
+      pass
+
+  nonsense_recs = preprocessor([headers]+clean_recs)
+  nonsense_recs, _ = postprocessor({"all":nonsense_recs}, headers)
+  train_supplement = nonsense_recs["all"][1:] # Ignore the duplicate headers
+  print len(train_supplement)
+
   write_uniq_csv(data, "all.csv")
 
   for i in xrange(num_splits):
@@ -53,9 +73,9 @@ def split_and_write():
     split_data, new_headers = postprocessor(split_data, headers)
 
     timestamp = int(time.time())
-    write_uniq_csv([new_headers]+split_data["train"],
+    write_uniq_csv([new_headers]+split_data["train"]+train_supplement,
                    "train_{}.csv".format(timestamp))
-    write_uniq_csv([new_headers]+split_data["test"],
+    write_uniq_csv([new_headers]+split_data["test"]+train_supplement,
                    "test_{}.csv".format(timestamp))
 
 
