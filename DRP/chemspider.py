56cfdd563edea2966668b0348d0b9c73f69f3b4e
Philip Adler <padler1@haverford.edu>
2015-07-09 14:59:22 -0400

    stripped out some bad practice code in chemspider.py

--- DRP/chemspider.py
+++ DRP/chemspider.py
@@ -1,36 +1,32 @@
 from models.CompoundEntry import CompoundEntry
 import chemspipy
 
-def chemspider_find(search_fields):
+def chemspider_find(search_terms):
 
-  for i in search_fields:
-    try:
+  for i in search_terms:
       query = chemspipy.find_one(i)
       return query
-    except Exception as e:
-      pass
-  return None
 
 
 def search_chemspider(val):
 
   # Variable Setup
-  chemspi_query = ""
   search_fields = ["CAS_ID", "compound"]
+  search_terms = []
 
   #Accept either a CompoundEntry object or a dict with the valid fields
 
   if type(val)==dict:
-    search_fields = [val.get(i) for i in search_fields if val.get(i)]
+    search_terms = [val.get(i) for i in search_fields if val.get(i)]
   elif type(val)==CompoundEntry:
-    query_criteria = [val.abbrev, val.compound]
+    query_terms = [val.compound]
   elif type(val)==list:
-    search_fields = val
+    search_terms = val
   else:
-    search_fields = [val]
+    search_terms = [val]
 
   # `result` is a chemspider object.
-  result = chemspider_find(search_fields)
+  result = chemspider_find(search_terms)
 
   return result
 
