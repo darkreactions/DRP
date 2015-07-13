a818d25587276b66b3064e6737e31e100e5ea6c1
Philip Adler <padler1@haverford.edu>
2015-07-13 09:52:51 -0400

    removed old comments from chempsider tests

--- DRP/tests/Chemspider.py
+++ DRP/tests/Chemspider.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python
-
-#REMEMBER when you create a new test case to add it to the suite() method, and then
-#to have that suite method called in AllTests.py
 import sys
 import TestConfig
 import DRP.chemspider as chemspider
 import DRP.models.CompoundEntry as CompoundEntry
 import unittest
 
-
 loadTests = unittest.TestLoader.loadTestsFromTestCase
 
 class ChemspiderFindFromName(unittest.TestCase):
-    #This class exemplifies the standard structure of a test. Check the documentation for 'rolling your own'
-
+    
     def runTest(self):
         ethanol = chemspider.chemspider_find(['ethanol'])
 	self.assertEqual(ethanol.csid,'682')	
@@ -49,7 +44,6 @@ class ChemspiderConflict(unittest.TestCase):
             chemspider.search_chemspider(['288-13-1', 'ethanol'])
 
 def suite():
-    #This function should be adjusted to contain the loadTests() function enacted on each test case.
     return unittest.TestSuite([
             loadTests(ChemspiderFindFromName),
             loadTests(ChemspiderFindFromCAS),
@@ -61,5 +55,4 @@ def suite():
 
 
 if __name__ == '__main__':
-    #Runs the test- a good way to check that this particular test set works without having to run all the tests.
     unittest.main()
