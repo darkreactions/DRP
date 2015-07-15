7716d38431ddfc03fe80710c75b96cd1a20d7f31
Philip Adler <padler1@haverford.edu>
2015-07-15 01:48:33 -0400

    added some addenda and docstringed the descriptorvalue class;

--- DRP/models/DescriptorValue.py
+++ DRP/models/DescriptorValue.py
@@ -1,13 +1,23 @@
+'''A module containign only the DescriptorValue class'''
 from django.db import models
+from Descriptor import Descriptor
+from Reaction import Reaction
+from Compound import Compound
 
-class DescriptorValue:
+class DescriptorValue(models.Model):
+  '''Contains Relationships between both Reactions and Compounds and their descriptors'''
 
   class Meta:
     app_label="DRP"
 
   descriptor = models.ForeignKey(Descriptor)
+  Reaction = models.ForeignKey(Reaction, null=True, unique=False, default=None)
+  Compound = models.ForeignKey(Compound, null=True, unique=False, default=None)
   booleanValue= models.BooleanField('Value if descriptor is a boolean', null=True)
   ordValue = models.PositiveIntegerField('Value if descriptor is an ordinal', null=True)
   catValue = models.Charfield('Value if descriptor is a category', max_length=200, null=True)
   numValue = models.FloatField('Value if descritpor is continuous', null=True)
   isPredicted=models.BooleanField()
+  '''Some stored descriptors will be predicted outcomes formed from the basis of a statistical model.
+  This provides that indication
+  '''
