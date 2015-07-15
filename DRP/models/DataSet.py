dc3e6d7283b73bd4ddf47be43b7d67e633a14659
Philip Adler <padler1@haverford.edu>
2015-07-15 01:38:43 -0400

    Docstringed the DataSet class

--- DRP/models/DataSet.py
+++ DRP/models/DataSet.py
@@ -1,7 +1,11 @@
+'''Module containing only the DataSet class'''
 from django.db import models
 from StatsModel import StatsModel
 
 class DataSet(models.Model):
+  '''The DataSet class describes the relationship between a statistical model and
+  the reactions used to either build or test that model.
+  '''
 
   Meta:
     app_label='DRP'
