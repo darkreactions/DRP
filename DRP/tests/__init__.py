dfd6f4ded6c86c6d3a1bcae92fea21b8e714605d
Philip Adler <padler1@haverford.edu>
2015-07-27 17:29:54 -0400

    adjusted tests __init__.py to account for TestConfig.py

--- DRP/tests/__init__.py
+++ DRP/tests/__init__.py
@@ -1,8 +1,1 @@
-from os import path
-import sys
-
-drpTestPath = path.dirname(os.realpath(__FILE__))
-drpPath = path.dirname(drpTestPath)
-
-if drpPath not in sys.path:
-    sys.path.append(drpPath)
+import TestConfig
