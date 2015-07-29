0861a887b3b321ae743fd9cadc082d8051e1d987
Philip Adler <padler1@haverford.edu>
2015-07-29 09:59:34 -0400

    adjusted settings imports to be standards compliant

--- DRP/models/ChemicalClass.py
+++ DRP/models/ChemicalClass.py
@@ -1,11 +1,11 @@
 '''A module containing only the ChemicalClass class'''
 from django.db import models
-from DRP.settings import CHEMICAL_CLASS_CHOICES
+from django.conf import settings
 
 class ChemicalClass(models.Model):
 
   class Meta:
     app_label='DRP'
 
-  label=models.CharField(max_length=30, unique=True, error_messages={'unique':'A chemical class with this label already exists'}, choices=CHEMICAL_CLASS_CHOICES)
+  label=models.CharField(max_length=30, unique=True, error_messages={'unique':'A chemical class with this label already exists'}, choices=settings.CHEMICAL_CLASS_CHOICES)
   description=models.CharField(max_length=20)
--- DRP/models/StatsModel.py
+++ DRP/models/StatsModel.py
@@ -1,7 +1,7 @@
 '''A module containing only the StatsModel class'''
 from django.db import models
 from StatsModelTag import StatsModelTag
-from DRP.settings import LIBRARY_CHOICES, TOOL_CHOICES
+from django.conf import settings
 
 class StatsModel(models.Model):
   '''A class for describing a statistical model generated to describe statistical models generated as
@@ -17,6 +17,6 @@ class StatsModel(models.Model):
   start_time=models.DateTimeField()
   end_time=models.DateTimeField(default=None, null=True)
   iterations=models.IntegerField()
-  library=models.CharField(max_length=200, choices=LIBRARY_CHOICES)
-  tool=models.CharField(max_length=200, choices=TOOL_CHOICES)
+  library=models.CharField(max_length=200, choices=settings.LIBRARY_CHOICES)
+  tool=models.CharField(max_length=200, choices=settings.TOOL_CHOICES)
   tags=models.ManyToManyField(StatsModelTag)
