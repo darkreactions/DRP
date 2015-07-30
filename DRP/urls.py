c4f0cc97d700215a984bfa74aca02873d66638c0
Philip Adler <padler1@haverford.edu>
2015-07-30 15:00:23 -0400

    added confirmation urls to urls.py

--- DRP/urls.py
+++ DRP/urls.py
@@ -21,6 +21,7 @@ urlpatterns = patterns('',
     url(r'^login.html$', login, {'template_name':'login.html'}, name='login'),
     (r'^logout.html$', logout, {'next_page':'home'}),
     url(r'^register.html$', DRP.views.register, name='register'),
+    url(r'^confirm.html$', DRP.views.confirm, name='confirm'),
 #    (r'^license.html$', DRP.views.license)
 #    (r'^join_lab.html$', DRP.views.joinLab)
 
