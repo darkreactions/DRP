744bd52d16a21758ef7408cafdf659941073e2e8
Philip Adler <padler1@haverford.edu>
2015-07-30 12:33:22 -0400

    added some user registration views

--- DRP/views/register.py
+++ DRP/views/register.py
@@ -1,4 +1,4 @@
-'''A module containing only the register view'''
+'''A module containing only the views for registration and confirmation'''
 from DRP.forms import UserCreationForm
 from django.template import RequestContext
 from DRP.Email import Email
@@ -6,20 +6,51 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.core.urlresolvers import reverse
 from django.utils.http import urlencode
+from django.template.loader import render_to_string
+from drp.forms import ConfirmationForm
+from DRP.models import ConfirmationCode
+from uuid import uuid4
+from django.core.exceptions import PermissionDenied
 
 def register(request):
   '''A view to permit new users to sign up.'''
-  redirected = False
+  
   if request.method=='POST':
     form = UserCreationForm(request.POST)
     if form.is_valid():
-      redirected = True
-      form.save()
-      if 'next' in request.GET.keys():
-        return redirect('{0}?next={1}'.format(reverse('login'), urlencode(request.GET['next'])))
-      else:
-        return redirect('login')
+      user = form.save()
+      user.active = False
+      user.save()
+      code = uuid4()
+      confCode = ConfirmationCode(user=user, code=code)
+      confCode.save()
+      emailText = render_to_string('confirmation_email.html', {'name':user.first_name + ' ' + user.second_name, 'code':code})
+      Email('Dark Reactions Project Registration', emailText, [user.email])
+      return render(request, 'register.html', {'form':form, 'submitted':True})
+    else:
+      return render(request, 'register.html', {'form':form, 'submitted':False})
   else:
     form = UserCreationForm()
-  if not redirected:
-    return render(request, 'register.html', RequestContext(request, {'form':form}))
+    return render(request, 'register.html', RequestContext(request, {'form':form, 'submitted':False}))
+
+def confirm(request):
+  '''A view to confirm sign-up, and to render the license agreement binding'''
+
+  if 'code' in request.GET.keys():
+    if request.method = 'POST':
+      form = ConfirmationForm(request, request.POST) 
+      if form.is_valid():
+        user = form.get_user()
+        if user.confirmationcode = request.GET['code']:
+          user.active = True
+          user.save()
+          return render(request, 'confirm.html', RequestContext(request, {'form':form, 'success':True, 'code':urlencode(request.GET['code']})))
+        else:
+          raise PermissionDenied()
+      else:
+        return render(request, 'confirm.html', RequestContext(request, {'form':form, 'success':False, 'code':urlencode(request.GET['code']}))
+    else:
+      form = ConfirmationForm(request)
+      render(request, 'confirm.html', RequestContext(request, {'form':form, 'success':False, 'code':urlencode(request.GET['code']}))
+  else:
+    raise PermissionDenied()
