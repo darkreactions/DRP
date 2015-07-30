b1ec66c6bd7ef0d59d30d88d2897e9a4a9b8799f
Philip Adler <padler1@haverford.edu>
2015-07-30 11:37:24 -0400

    removed old tests

--- DRP/tests/TestConfig.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-import sys, os
-
-appPath = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
-sys.path.append(appPath)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'DRP.settings'
