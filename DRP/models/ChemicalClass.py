659649437dc653f8035de8ea8653220448a62b66
Philip Adler <padler1@haverford.edu>
2015-08-06 15:05:36 -0400

    added a verbose plural to ChemicalClass

--- DRP/models/ChemicalClass.py
+++ DRP/models/ChemicalClass.py
@@ -6,6 +6,7 @@ class ChemicalClass(models.Model):
 
   class Meta:
     app_label='DRP'
+    verbose_name_plural='Chemical Classes'
 
   label=models.CharField(max_length=30, unique=True, error_messages={'unique':'A chemical class with this label already exists'}, choices=settings.CHEMICAL_CLASS_CHOICES)
   description=models.CharField(max_length=20)
