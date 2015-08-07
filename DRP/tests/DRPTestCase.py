dc4a99963707c6cfa9deb01cd1f5c4b9cee80a05
Philip Adler <padler1@haverford.edu>
2015-08-07 12:11:40 -0400

    added database clearing methods to DRPTestCase for convenience

--- DRP/tests/DRPTestCase.py
+++ DRP/tests/DRPTestCase.py
@@ -1,6 +1,8 @@
 '''A module containing only the DRPTestCase class'''
 from unittest import TestCase 
 from django.conf import settings
+import DRP.models as models 
+from django.contrib.auth.models import User
 
 class DRPTestCase(TestCase):
   '''A quick and dirty safety valve to stop people accidentally running database tests in production environments
@@ -11,4 +13,13 @@ class DRPTestCase(TestCase):
     if not settings.TESTING:
       raise RuntimeError('Testing environment not enabled')
     else:
+      models.Compound.objects.all().delete()
+      models.ChemicalClass.objects.all().delete()
+      models.LabGroup.objects.objects.all().delete()
+      models.ConfirmationCode.objects.all().delete()
+      models.LicenseAgreement.objects.all()
+      models.License.objects.all().delete()
+      Users.objects.all().exclude(username='root').delete()
+      
+      
       super(DRPTestCase, self).__init__(*args, **kwargs)
