5027adbf07bf864f3f0febd91ebfb39c5d6d15e4
Philip Adler <padler1@haverford.edu>
2015-08-28 14:45:41 -0400

    added the compound filtering form to the forms subpackage init

--- DRP/forms/__init__.py
+++ DRP/forms/__init__.py
@@ -4,5 +4,5 @@ Classes.
 '''
 from LabGroup import LabGroupForm, LabGroupJoiningForm, LabGroupSelectionForm
 from Contact import ContactForm
-from compound import CompoundForm, CompoundAdminForm, CompoundEditForm, CompoundDeleteForm, CompoundUploadForm
+from compound import CompoundForm, CompoundAdminForm, CompoundEditForm, CompoundDeleteForm, CompoundUploadForm, CompoundFilterForm
 from authentication import ConfirmationForm, LicenseAgreementForm, UserCreationForm
