dffaef3b592ae0dc867ebed71971b1930351070c
Philip Adler <padler1@haverford.edu>
2015-09-03 12:40:26 -0400

    removed a superfluous setting

--- DRP/settings_example.py
+++ DRP/settings_example.py
@@ -7,7 +7,7 @@ SERVER_NAME = '' #TODO: replace this using sites application.
 TESTING = False #tells the system that you are happy for tests, some of which run on the live database to be run. DO NOT SET TO TRUE IN PRODUCTION.
 EXTERNAL_HTML_VALIDATOR = 'http://validator.w3.org/nu/' #The external html validator. You shouldn't need to change this.
 
-CHEMSPIDER_TOKEN = '39a17d3f-0353-4b04-be4e-eb6cd73e0722'
+CHEMSPIDER_TOKEN = ''
 
 LOGIN_REDIRECT_URL = '/'
 
@@ -241,7 +241,6 @@ CACHES = {
 
 #Force users to log out when the browser is closed.
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
-CHEMICAL_CLASS_CHOICES=()
 LIBRARY_CHOICES=()
 TOOL_CHOICES=()
 
