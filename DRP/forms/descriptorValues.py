74e8d86b464c57192b617b26fcd4ebd60b85168f
Philip Adler <padler1@haverford.edu>
2015-09-09 13:12:20 -0400

    added descriptor value form aspect

---
+++ DRP/forms/descriptorValues.py
@@ -0,0 +1,32 @@
+
+from django import forms
+from django.forms import modelformset_factory
+from DRP.models import PerformedReaction, OrdRxnDescriptorValue, CompoundQuantity
+from DRP.models import NumRxnDescriptorValue, BoolRxnDescriptorValue, CatRxnDescriptorValue
+from DRP.models import NumRxnDescriptor, BoolRxnDescriptor, CatRxnDescriptor, OrdRxnDescriptor
+
+def descriptorValueFormFactory(modelClass, descriptorClass):
+
+  class DescriptorValueForm(forms.ModelForm):
+
+    class Meta:
+      model=modelClass
+      fields=('descriptor', 'value')
+
+    def __init__(self, *args, **kwargs):
+      super(DescriptorValueForm, self).__init__(*args, **kwargs)
+      self.fields['descriptor'].queryset = descriptorClass.objects.filter(calculatorSoftware='manual')
+
+  return DescriptorValueForm
+
+factory = descriptorValueFormFactory
+
+NumRxnDescValForm = factory(NumRxnDescriptorValue, NumRxnDescriptor)
+OrdRxnDescValForm = factory(OrdRxnDescriptorValue, OrdRxnDescriptor)
+BoolRxnDescValForm = factory(BoolRxnDescriptorValue, BoolRxnDescriptor)
+CatRxnDescValForm = factory(CatRxnDescriptorValue, CatRxnDescriptor) 
+
+NumRxnDescValFormSet = modelformset_factory(NumRxnDescriptorValue, form=NumRxnDescValForm, can_delete=True, prefix='num')
+OrdRxnDescValFormSet = modelformset_factory(OrdRxnDescriptorValue, form=OrdRxnDescValForm, can_delete=True, prefix='ord')
+BoolRxnDescValFormSet = modelformset_factory(BoolRxnDescriptorValue, form=BoolRxnDescValForm, can_delete=True, prefix='bool')
+CatRxnDescValFormSet = modelformset_factory(CatRxnDescriptorValue, form=CatRxnDescValForm, can_delete=True, prefix='cat')
