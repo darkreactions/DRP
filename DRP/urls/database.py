cfee1d3643108d35a44c040aac8725b4c9a1e7e4
Philip Adler <padler1@haverford.edu>
2015-09-16 16:35:30 -0400

    added in urls and links for editing reactions

--- DRP/urls/database.py
+++ DRP/urls/database.py
@@ -6,6 +6,7 @@ import DRP.views
 urls = patterns('',
   url('^$', DRP.views.reaction.ListPerformedReactions.as_view(), name='reactionlist'),
   url('^add.html', DRP.views.reaction.createReaction, name='newReaction'),
+  url('^edit_(?P<pk>\d+).html', DRP.views.compound.editReaction, name='editReaction'),
   url('^select_viewing_group.html', DRP.views.selectGroup, name='selectGroup'),
   url('^compoundguide(?P<filetype>.csv|.html|.arff|/)$', DRP.views.compound.ListCompound.as_view(), name='compoundguide'),
   url('^compoundguide/search(?P<filetype>.html|.csv|.arff)$', DRP.views.compound.ListCompound.as_view(), name='compoundSearch'),
