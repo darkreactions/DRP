613601a098aac2d1e52408ca5275c49627b88a29
Philip Adler <padler1@haverford.edu>
2015-09-25 12:23:39 -0400

    pep257.

--- DRP/admin.py
+++ DRP/admin.py
@@ -1,3 +1,5 @@
+"""Contains admin information for Django."""
+
 from django.contrib import admin
 from models import LabGroup, License, Compound, ChemicalClass, CompoundRole
 from models import PerformedReaction, OrdRxnDescriptor, NumRxnDescriptor
@@ -10,10 +12,14 @@ from forms import CatRxnDescriptorForm, CatDescPermittedValueForm
 
 
 class LabGroupAdmin(admin.ModelAdmin):
+
+    """Specifies a form to use for administrating lab groups."""
+
     form = LabGroupForm
 
 
 def licenseSnippet(license):
+    """Return a snippet of a license."""
     return license.text[:100] + '...'
 
 licenseSnippet.short_description = 'License Snippet'
@@ -21,26 +27,36 @@ licenseSnippet.short_description = 'License Snippet'
 
 class LicenseAdmin(admin.ModelAdmin):
 
+    """Provides a more specific display for licenses in the django admin."""
+
     list_display = (licenseSnippet, 'effectiveDate')
 
 
 class CompoundAdmin(admin.ModelAdmin):
-    form = CompoundAdminForm
 
+    """Specifies the form and list display of a compound in django."""
+
+    form = CompoundAdminForm
     list_display = ('abbrev', 'name', 'CSID', 'custom', 'labGroup')
 
 
 class ChemicalClassAdmin(admin.ModelAdmin):
+
+    """Specifies a list display for chemical classes."""
 
     list_display = ('label', 'description')
 
 
 class CompoundRoleAdmin(admin.ModelAdmin):
 
+    """Specifies a list display for compound roles."""
+
     list_display = ('label', 'description')
 
 
 class PerformedRxnAdmin(admin.ModelAdmin):
+
+    """Defines a list display and form for Performed Reactions in django."""
 
     list_display = ('reference', 'user', 'labGroup', 'performedDateTime')
     form = PerformedRxnAdminForm
@@ -48,16 +64,21 @@ class PerformedRxnAdmin(admin.ModelAdmin):
 
 class BoolRxnDescriptorAdmin(admin.ModelAdmin):
 
+    """Specifies a form and list display for custom boolean rxn descriptors."""
+
     list_display = ('heading', 'name')
     form = BoolRxnDescriptorForm
 
 
 class NumRxnDescriptorAdmin(admin.ModelAdmin):
 
+    """Specifies a form, list display for custom numerical rxn descriptors."""
+
     list_display = ('heading', 'name', 'maximum', 'minimum')
     form = NumRxnDescriptorForm
 
     def get_form(self, request, obj=None, **kwargs):
+        """Prevent maximum and minimum values from being changed."""
         if obj is not None:
             kwargs['exclude'] = ('maximum', 'minimum')
         return super(NumRxnDescriptorAdmin, self).get_form(
@@ -66,10 +87,13 @@ class NumRxnDescriptorAdmin(admin.ModelAdmin):
 
 class OrdRxnDescriptorAdmin(admin.ModelAdmin):
 
+    """Specifies a form, list display for custom numerical rxn descriptors."""
+
     list_display = ('heading', 'name', 'maximum', 'minimum')
     form = OrdRxnDescriptorForm
 
     def get_form(self, request, obj=None, **kwargs):
+        """Prevent maximum and minimum values from being changed."""
         if obj is not None:
             kwargs['exclude'] = ('maximum', 'minimum')
         return super(OrdRxnDescriptorAdmin, self).get_form(
@@ -77,11 +101,17 @@ class OrdRxnDescriptorAdmin(admin.ModelAdmin):
 
 
 class CatRxnDescriptorAdmin(admin.ModelAdmin):
+
+    """Specifies a form, list display for custom category rxn descriptors."""
+
     list_display = ('heading', 'name')
     form = CatRxnDescriptorForm
 
 
 class CatDescPermValAdmin(admin.ModelAdmin):
+
+    """Specifies a form, list display for permitted categorical values."""
+
     list_display = ('value', 'descriptor')
     form = CatDescPermittedValueForm
 
