7867dd99781451f4ac9b06e045802d55084f0602
Philip Adler <padler1@haverford.edu>
2015-10-06 14:44:33 -0400

    inorganic descriptors are now coded but  untested

--- DRP/plugins/rxndescriptors/drp.py
+++ DRP/plugins/rxndescriptors/drp.py
@@ -8,7 +8,7 @@ import DRP
 
 atoms = DRP.chemical_data.elements
 
-_descriptorDict = {
+_descriptorDict = { 
     'numberInorganic':
         {
             'type': 'num',
@@ -17,57 +17,48 @@ _descriptorDict = {
             'calculatorSoftwareVersion': '0.02',
             'maximum': None,
             'minimum': 0,
-        }
-    'inorgAtomIonizationMax':
-        {
-            'type': 'num',
-            'name': 'Maximum atomic ionization energy among inorganic species',
-            'calculatorSoftware': 'DRP',
-            'calculatorSoftwareversion':'0.02',
-            'maximum': None,
-            'minimum': None
-        }
-    'inorgAtomIonizationMin':
-        {
-            'type': 'num',
-            'name': 'Minimum atomic ionization energy among inorganic species',
-            'calculatorSoftware': 'DRP',
-            'calculatorSoftwareversion':'0.02',
-            'maximum': None,
-            'minimum': None
-        }
-    'inorgAtomIonizationMean':
-        {
-            'type': 'num',
-            'name': 'Mean atomic ionization energy among inorganic species',
-            'calculatorSoftware': 'DRP',
-            'calculatorSoftwareversion':'0.02',
-            'maximum': None,
-            'minimum': None
-        }
-    'inorgAtomIonizationGeom'
-        {
-            'type': 'num',
-            'name': 'Geometric average atomic ionization energy among inorganic species',
-            'calculatorSoftware': 'DRP',
-            'calculatorSoftwareversion':'0.02',
-            'maximum': None,
-            'minimum': None
-        }
-    'inorgAtomIonizationMaxWeighted'
-        {
-            'type': 'num',
-            'name': 'Maximum atomic ionization energy among inorganic species, stoichiometrically and molar weighted.',
-            'calculatorSoftware': 'DRP',
-            'calculatorSoftwareversion':'0.02',
-            'maximum': None,
-            'minimum': None
-        }
+        },
 }
 
-descriptorDict = setup(_descriptorDict)
+#The following adds descriptors to the dictionary in an automated way to save on voluminous code
 
-InorgElements = {}
+#Inorganic descriptors
+inorgAtomicProperties = (
+    'ionization_energy',
+    'electron_affinity',
+    'pauling_electronegativity',
+    'pearson_electronegativity',
+    'hardness',
+    'atomic_radius'
+)
+weightings = (
+    ('', ''), 
+    ('MolWeighted', ' weighted by the molarity of the relevant atoms'),
+    ('StoichWeighted', ' weighted in each species stoichiometrically, and aggregated uniformly')
+)
+aggregations = (
+    ('Max', 'Maximum'), 
+    ('Min', 'Minimum'),
+    ('Mean', 'Mean Average'),
+    ('Geom', 'Geometric Average')
+)
+
+
+for prop in inorgAtomicProperties:
+    stem = 'drpInorgAtom' + prop.title().replace('_', '') 
+    for weighting in weightings:
+        for agg in aggregations:
+             _descriptorDict[stem + weighting[0] + agg[0]] = {
+                'type':'num',
+                'name': '{} {}{}.'.format(agg[1], prop.replace('_', ' '), weighting[1]),
+                'calculatorSoftware':'DRP',
+                'calculatorSoftwareVersion':'0.02',
+                'maximum':None,
+                'minimum':None
+            }
+
+#Set up the actual descriptor dictionary.
+descriptorDict = setup(_descriptorDict)
 
 for element, info in elements.items():
     if (element == 'Se') or (info['group'] in range(3, 13)) or ((info['group'] > 12) and ((not info['nonmetal']) or info['metalloid'])):
@@ -75,9 +66,11 @@ for element, info in elements.items():
 
 
 def calculate(reaction):
+    """Calculate the descriptors for this plugin."""
     num = DRP.models.NumRxnDescriptorValue
     inorgCompoundQuantities = DRP.models.CompoundQuantity.objects.filter(reaction=reaction, role__label='Inorg')  # These two lines get the inorganic compounds for htis reaction.
     sumInorgAmount = sum(quantity.amount for quantity in inorgCompoundQuantities)
+
 
     MAX = 1
     MIN = 2
@@ -87,6 +80,7 @@ def calculate(reaction):
     WSTOICH = 2
 
     def inorgAtomicAggregate(propertyLabel, function, weighting=None):
+        """Figure out the aggregation for inorganic atom-based properties. This is done a lot so this saves on code repetition."""
         if weighting is None:
             if function == MAX:
                 return max(max(inorgElements[element][propertyLabel] for element in quantity.compound.elements) for quantity in inorgCompoundQuantities)
@@ -106,15 +100,15 @@ def calculate(reaction):
                 return min(min(inorgElements[element][propertyLabel]*(quantity.amount/sumInorgAmount)*info['stoichiometry'] for element, info in quantity.compound.elements)
                                             for quantity in inorgCompoundQuantities)
             elif function == MEAN:
-                return mean(chain(inorgElements[element]['ionization_energy']*(quantity.amount/sumInorgAmount)*info['stoichiometry'] for element, info in quantity.compound.elements)
+                return mean(chain(inorgElements[element][propertyLabel]*(quantity.amount/sumInorgAmount)*info['stoichiometry'] for element, info in quantity.compound.elements)
                                             for quantity in inorgCompoundQuantities)
             elif function == GMEAN:
-                return gmean(chain(inorgElements[element]['ionization_energy']*(quantity.amount/sumInorgAmount)*info['stoichiometry'] for element, info in quantity.compound.elements)
+                return gmean(chain(inorgElements[element][propertyLabel]*(quantity.amount/sumInorgAmount)*info['stoichiometry'] for element, info in quantity.compound.elements)
                                             for quantity in inorgCompoundQuantities))
             else:
                 raise TypeError('Unrecognised function selection constant')
         elif weighting == WSTOICH: 
-            values = (wmean((inorgElements[element]['ionization_energy'] for element in quantity.compound.elements), 
+            values = (wmean((inorgElements[element][propertyLabel] for element in quantity.compound.elements), 
                                 weights=(info['stoichiometry'] for element, info in quantity.compound.elements.items()))
                                             for quantity in inorgCompoundQuantities)
             if function == MAX:
@@ -129,7 +123,6 @@ def calculate(reaction):
                 raise TypeError('Unrecognised function selection constant')
         else:
             raise TypeError('Unrecognised weighting scheme')
-            
 
     # Number of inorganic components
     num.objects.get_or_create(
@@ -137,73 +130,66 @@ def calculate(reaction):
                             descriptor=descriptorDict['numberInorganic'],
                             value=inorgCompounds.count())
 
-    # Maximum ionisation potential across all relevant inorganic atom types in the reaction.
-    num.objects.get_or_create(
-                            reaction=reaction,
-                            descriptor=descriptorDict['drpInorgMetalAtomIonizationMax'],
-                            value=inorgAtomicAggregate('ionization_energy', MAX)
+    for prop in inorgAtomicProperties:
+        #  Calculate the inorganic atomic properties, weight them where needed and insert them into the database.
+        stem = 'drpInorgAtom' + prop.title().replace('_', '')
 
-    # Minimum ionisation potential across all relevant inorganic atom types in the reaction.
-    num.objects.get_or_create(
-                            reaction=reaction,
-                            descriptor=descriptorDict['drpInorgMetalAtomIonizationMin'],
-                            value=inorgAtomicAggregate('ionization_energy', MIN)
-
-    # Mean ionisation potential across all relevant inorganic atom types in the reaction
-    num.objects.get_or_create(
-                            reaction=reaction,
-                            descriptor=descriptorDict['drpInorgMetalAtomIonizationMean'],
-                            value=inorgAtomicAggregate('ionization_energy', MEAN)
-
-    # Geometric Average of the ionisation potential across all relevant inorganic atom types in the reaction
-    num.objects.get_or_create(
-                            reaction=reaction,
-                            descriptor=descriptorDict['drpInorgMetalAtomIonizationGeom'],
-                            value=inorgAtomicAggregate('ionization_energy', GMEAN)
-   
-    # Maximum molarity-weighted value of ionisation potential across all relevant inorganic atoms in the reaction
-    num.objects.get_or_create(
-                            reaction=reaction,
-                            descriptor=descriptorDict['drpInorgMetalAtomIonizationMaxMolWeighted'],
-                            value=inorgAtomicAggregate('ionization_energy', MAX, WMOL) 
-
-    # Minimum molarity-weighted value of ionisation potential across all relevant inorganic atoms in the reaction
-    num.objects.get_or_create(
-                            reaction=reaction,
-                            descriptor=descriptorDict['drpInorgMetalAtomIonizationMinMolWeighted'],
-                            value=inorgAtomicAggregate('ionization_energy', MIN, WMOL)
-
-    # Average molarity-weighted value of ionisation potential across all relevant inorganic atoms in the reaction
-    num.objects.get_or_create(
-                            reaction=reaction,
-                            descriptor=descriptorDict['drpInorgMetalAtomIonizationMeanMolWeighted'],
-                            value=inorgAtomicAggregate('ionization_energy', MEAN, WMOL)
-
-    # Geometric Average molarity-weighted value of ionisation potential across all relevant inorganic atoms in the reaction
-    num.objects.get_or_create(
-                            reaction=reaction,
-                            descriptor=descriptorDict['drpInorgMetalAtomIonizationGeomMolWeighted'],
-                            value=inorgAtomicAggregate('ionization_energy', GMEAN, WMOL)
-
-    # Maximum (stoichiometrically weighted average value of ionisation potential across all relevant inorganic atoms in each species) across all species in the reaction
-    num.objects.get_or_create(
-                            reaction=reaction,
-                            descriptor=descriptorDict['drpInorgMetalAtomIonizationMaxStoichWeighted'],
-                            value=inorgAtomicAggregate('ionization_energy', MAX, WMOL)
-    # Minimum (stoichiometrically weighted average value of ionisation potential across all relevant inorganic atoms in each species) across all species in the reaction
-    num.objects.get_or_create(
-                            reaction=reaction,
-                            descriptor=descriptorDict['drpInorgMetalAtomIonizationMinStoichWeighted'],
-                            value=inorgAtomicAggregate('ionization_energy', MIN, WMOL)
-
-    # Mean (stoichiometrically weighted value of ionisation potential across all relevant inorganic atoms in each species) across all species in the reaction
-    num.objects.get_or_create(
-                            reaction=reaction,
-                            descriptor=descriptorDict['drpInorgMetalAtomIonizationMeanStoichWeighted'],
-                            value=inorgAtomicAggregate('ionization_energy', MEAN, WSTOICH)
-
-    # Geometric Mean (stoichiometrically weighted value of ionisation potential across all relevant inorganic atoms in each species) across all species in the reaction
-    num.objects.get_or_create(
-                            reaction=reaction,
-                            descriptor=descriptorDict['drpInorgMetalAtomIonizationGeomStoichWeighted'],
-                            value=inorgAtomicAggregate('ionization_energy', GMEAN, WSTOICH)
+        num.objects.get_or_create(
+                                reaction=reaction,
+                                descriptor=descriptorDict[stem + 'Max'],
+                                value=inorgAtomicAggregate(prop, MAX))
+    
+        num.objects.get_or_create(
+                                reaction=reaction,
+                                descriptor=descriptorDict[stem + 'Min'],
+                                value=inorgAtomicAggregate(prop, MIN))
+    
+        num.objects.get_or_create(
+                                reaction=reaction,
+                                descriptor=descriptorDict[stem + 'Mean'],
+                                value=inorgAtomicAggregate(prop, MEAN))
+    
+        num.objects.get_or_create(
+                                reaction=reaction,
+                                descriptor=descriptorDict[stem + 'Geom'],
+                                value=inorgAtomicAggregate(prop, GMEAN))
+       
+        num.objects.get_or_create(
+                                reaction=reaction,
+                                descriptor=descriptorDict[stem + 'MolWeightedMax'],
+                                value=inorgAtomicAggregate(prop, MAX, WMOL)) 
+    
+        num.objects.get_or_create(
+                                reaction=reaction,
+                                descriptor=descriptorDict[stem + 'MolWeightedMax'],
+                                value=inorgAtomicAggregate(prop, MIN, WMOL))
+    
+        num.objects.get_or_create(
+                                reaction=reaction,
+                                descriptor=descriptorDict[stem + 'MolWeightedMean'],
+                                value=inorgAtomicAggregate(prop, MEAN, WMOL))
+    
+        num.objects.get_or_create(
+                                reaction=reaction,
+                                descriptor=descriptorDict[stem + 'MolWeightedGeom'],
+                                value=inorgAtomicAggregate(prop, GMEAN, WMOL))
+    
+        num.objects.get_or_create(
+                                reaction=reaction,
+                                descriptor=descriptorDict[stem + 'StoichWeightedMax'],
+                                value=inorgAtomicAggregate(prop, MAX, WMOL))
+    
+        num.objects.get_or_create(
+                                reaction=reaction,
+                                descriptor=descriptorDict[stem + 'StoichWeightedMin'],
+                                value=inorgAtomicAggregate(prop, MIN, WMOL))
+    
+        num.objects.get_or_create(
+                                reaction=reaction,
+                                descriptor=descriptorDict[stem + 'StoichWeightedMean'],
+                                value=inorgAtomicAggregate(prop, MEAN, WSTOICH))
+    
+        num.objects.get_or_create(
+                                reaction=reaction,
+                                descriptor=descriptorDict[stem + 'StoichWeightedGeom'],
+                                value=inorgAtomicAggregate(prop, GMEAN, WSTOICH))
