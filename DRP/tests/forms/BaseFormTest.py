0d3aeafb63b33a9a3e95df4be4ce8748bebfc60d
Philip Adler <padler1@haverford.edu>
2015-10-14 14:41:12 -0400

    fixed tests for compound filter formset

--- DRP/tests/forms/BaseFormTest.py
+++ DRP/tests/forms/BaseFormTest.py
@@ -18,8 +18,13 @@ class BaseFormTest(DRPTestCase):
     '''Test that the form does validate'''
     valid = self.form.is_valid()
     errString = ''
-    for e, m in self.form.errors.items():
-      errString += '{0}: {1}\n'.format(e, m)
+    if hasattr(self.form.errors, 'items'):
+        for e, m in self.form.errors.items():
+            errString += '{0}: {1}\n'.format(e, m)
+    else:
+        for s in self.form.errors:
+            for e, m in s.items():
+                errString += '{0}: {1}\n'.format(e, m)
     errString += ' Submitted data = {0}'.format(self.formData)
     self.assertTrue(valid, errString)
 
