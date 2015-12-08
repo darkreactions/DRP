7ed8798170b51670ae6463c2c00f6b377c44aeda
Philip Adler <padler1@haverford.edu>
2015-12-08 11:09:17 -0500

    began work on pulling apart old decorators so that we can easily test data importing procedures

--- DRP/tests/decorators.py
+++ DRP/tests/decorators.py
@@ -9,271 +9,323 @@ import os
 
 
 def createsRxnDescriptor(heading, descriptorType, options={}):
-  '''A class decorator that creates a reaction using pre-existing compounds
-     with pre-existing compoundRoles.'''
-  def _createsRxnDescriptor(c):
-    _oldSetup = c.setUp
-    _oldTearDown = c.tearDown
+    '''A class decorator that creates a reaction descriptor'''
+    def _createsRxnDescriptor(c):
+        _oldSetup = c.setUp
+        _oldTearDown = c.tearDown
 
-    descriptor = None
-    for constructor, descriptorVal in rxnDescriptorPairs:
-      if descriptorType == constructor.__name__:
-        descriptor = constructor()
+        descriptor = None
+        for constructor, descriptorVal in rxnDescriptorPairs:
+            if descriptorType == constructor.__name__:
+                descriptor = constructor()
 
-    if not descriptor:
-      error = "Descriptor type \"{}\" unknown to descriptor.".format(descriptorType)
-      raise NotImplementedError(error)
+        if not descriptor:
+            error = "Descriptor type \"{}\" unknown to descriptor.".format(descriptorType)
+            raise NotImplementedError(error)
 
-    def setUp(self):
-      descriptor.heading = heading
-      descriptor.name = heading
+        def setUp(self):
+            descriptor.heading = heading
+            descriptor.name = heading
 
-      for key, val in options.items():
-        setattr(descriptor, key, val)
+            for key, val in options.items():
+                setattr(descriptor, key, val)
 
-      descriptor.save()
+            descriptor.save()
 
-      _oldSetup(self)
+            _oldSetup(self)
 
-    def tearDown(self):
-      descriptor.delete()
-      _oldTearDown(self)
+        def tearDown(self):
+            descriptor.delete()
+            _oldTearDown(self)
 
-    c.setUp = setUp
-    c.tearDown = tearDown
-    return c
-  return _createsRxnDescriptor
-
-
-def createsPerformedReaction(labTitle, username, compoundAbbrevs, compoundRoles, compoundAmounts, descriptorDict):
-  '''A class decorator that creates a reaction using pre-existing compounds
-     with pre-existing compoundRoles.'''
-  def _createsPerformedReaction(c):
-    _oldSetup = c.setUp
-    _oldTearDown = c.tearDown
-
-    reaction = PerformedReaction()
-    compoundQuantities = []
-    descriptorVals = []
-
-    def setUp(self):
-      labGroup=LabGroup.objects.get(title=labTitle)
-      reaction.labGroup = labGroup
-
-      user=User.objects.get(username=username)
-      reaction.user = user
-
-      reaction.public = False
-
-      reaction.save()
-
-      for abbrev, role, quantity in zip(compoundAbbrevs, compoundRoles, compoundAmounts):
-        compound = Compound.objects.get(labGroup=labGroup, abbrev=abbrev)
-        compoundRole = CompoundRole.objects.get(label=role)
-        compoundQuantity = CompoundQuantity(compound=compound, reaction=reaction,
-                                            role=compoundRole, amount=quantity)
-        compoundQuantity.save()
-
-        compoundQuantities.append(compoundQuantity)
-
-      for descriptorHeading,val in descriptorDict.items():
-        descriptor, descriptorVal = getRxnDescriptorAndEmptyVal(descriptorHeading)
-
-        descriptorVal.descriptor = descriptor
-        descriptorVal.value = val
-        descriptorVal.reaction = reaction
-        descriptorVal.save()
-
-        descriptorVals.append(descriptorVal)
-
-      _oldSetup(self)
-
-    def tearDown(self):
-      _oldTearDown(self)
-
-      for cq in compoundQuantities:
-        cq.delete()
-
-      for descriptorVal in descriptorVals:
-        descriptorVal.delete()
-
-      TrainingSet.objects.filter(reaction=reaction).delete()
-      TestSet.objects.filter(reactions__in=[reaction]).delete()
-      reaction.delete()
+        c.setUp = setUp
+        c.tearDown = tearDown
+        return c
+    return _createsRxnDescriptor
 
 
-    c.setUp = setUp
-    c.tearDown = tearDown
-    return c
-  return _createsPerformedReaction
+def createsPerformedReaction(labTitle, username, reference, compoundAbbrevs, compoundRoles, compoundAmounts, descriptorDict={}, duplicateRef=None):
+    '''A class decorator that creates a reaction using pre-existing compounds
+          with pre-existing compoundRoles.'''
+    def _createsPerformedReaction(c):
+        _oldSetup = c.setUp
+        _oldTearDown = c.tearDown
+
+        reaction = PerformedReaction()
+        compoundQuantities = []
+        descriptorVals = []
+
+        def setUp(self):
+            labGroup=LabGroup.objects.get(title=labTitle)
+            reaction.labGroup = labGroup
+
+            user=User.objects.get(username=username)
+            reaction.user = user
+            reaction.reference = reference
+            reaction.public = False
+            if PerformedReaction.objects.filter(labGroup=labGroup, reference=duplicateRef).exists():
+                reaction.duplicateOf = PerformedReaction.objects.get(labGroup=labGroup, reference=duplicateRef)
+
+            reaction.save()
+
+            for abbrev, role, quantity in zip(compoundAbbrevs, compoundRoles, compoundAmounts):
+                compound = Compound.objects.get(labGroup=labGroup, abbrev=abbrev)
+                compoundRole = CompoundRole.objects.get(label=role)
+                compoundQuantity = CompoundQuantity(compound=compound, reaction=reaction,
+                                                                                        role=compoundRole, amount=quantity)
+                compoundQuantity.save()
+
+                compoundQuantities.append(compoundQuantity)
+
+            for descriptorHeading,val in descriptorDict.items():
+                descriptor, descriptorVal = getRxnDescriptorAndEmptyVal(descriptorHeading)
+
+                descriptorVal.descriptor = descriptor
+                descriptorVal.value = val
+                descriptorVal.reaction = reaction
+                descriptorVal.save()
+
+                descriptorVals.append(descriptorVal)
+
+            _oldSetup(self)
+
+        def tearDown(self):
+            _oldTearDown(self)
+
+            for cq in compoundQuantities:
+                cq.delete()
+
+            for descriptorVal in descriptorVals:
+                descriptorVal.delete()
+
+            TrainingSet.objects.filter(reaction=reaction).delete()
+            TestSet.objects.filter(reactions__in=[reaction]).delete()
+            reaction.delete()
 
 
-def createsUser(username, password):
-  '''A class decorator that creates a user'''
+        c.setUp = setUp
+        c.tearDown = tearDown
+        return c
+    return _createsPerformedReaction
 
-  def _createsUser(c):
 
-    _oldSetup = c.setUp
-    _oldTearDown = c.tearDown
+def createsUser(username, password, is_superuser=False):
+    '''A class decorator that creates a user'''
 
-    def setUp(self):
-      user = User.objects.create_user(username=username, password=password)
-      user.save()
-      _oldSetup(self)
+    def _createsUser(c):
 
-    def tearDown(self):
-      _oldTearDown(self)
-      User.objects.filter(username=username).delete()
+        _oldSetup = c.setUp
+        _oldTearDown = c.tearDown
 
-    c.setUp = setUp
-    c.tearDown = tearDown
-    return c
-  return _createsUser
+        def setUp(self):
+            user = User.objects.create_user(username=username, password=password)
+            user.is_superuser = is_superuser
+            user.save()
+            _oldSetup(self)
+
+        def tearDown(self):
+            _oldTearDown(self)
+            User.objects.filter(username=username).delete()
+
+        c.setUp = setUp
+        c.tearDown = tearDown
+        return c
+    return _createsUser
 
 def createsCompoundRole(label, description):
 
-  def _createsCompoundRole(c):
-    _oldSetup = c.setUp
-    _oldTearDown = c.tearDown
+    def _createsCompoundRole(c):
+        _oldSetup = c.setUp
+        _oldTearDown = c.tearDown
 
-    role = CompoundRole()
+        role = CompoundRole()
 
-    def setUp(self):
-      role.label = label
-      role.description = description
-      role.save()
-      _oldSetup(self)
+        def setUp(self):
+            role.label = label
+            role.description = description
+            role.save()
+            _oldSetup(self)
 
-    def tearDown(self):
-      _oldTearDown(self)
-      role.delete()
+        def tearDown(self):
+            _oldTearDown(self)
+            role.delete()
 
-    c.setUp = setUp
-    c.tearDown = tearDown
-    return c
-  return _createsCompoundRole
+        c.setUp = setUp
+        c.tearDown = tearDown
+        return c
+    return _createsCompoundRole
 
 
 def createsCompound(abbrev, csid, classLabel, labTitle, custom=False):
 
-  def _createsCompound(c):
+    def _createsCompound(c):
 
-    _oldSetup = c.setUp
-    _oldTearDown = c.tearDown
+        _oldSetup = c.setUp
+        _oldTearDown = c.tearDown
 
-    compound = Compound(abbrev=abbrev, CSID=csid, custom=custom)
+        compound = Compound(abbrev=abbrev, CSID=csid, custom=custom)
 
-    def setUp(self):
-      compound.labGroup=LabGroup.objects.get(title=labTitle)
-      compound.save()
-      for c in ChemicalClass.objects.filter(label=classLabel):
-        compound.chemicalClasses.add(c)
-      compound.save()
-      _oldSetup(self)
+        def setUp(self):
+            compound.labGroup=LabGroup.objects.get(title=labTitle)
+            compound.save()
+            for c in ChemicalClass.objects.filter(label=classLabel):
+                compound.chemicalClasses.add(c)
+            compound.save()
+            _oldSetup(self)
 
-    def tearDown(self):
-      _oldTearDown(self)
-      compound.delete()
+        def tearDown(self):
+            _oldTearDown(self)
+            compound.delete()
 
-    c.setUp = setUp
-    c.tearDown = tearDown
-    return c
-  return _createsCompound
+        c.setUp = setUp
+        c.tearDown = tearDown
+        return c
+    return _createsCompound
 
 def createsChemicalClass(label, description):
-  '''A class decorator that creates a test chemical class for the addition of compounds into the database'''
+    '''A class decorator that creates a test chemical class for the addition of compounds into the database'''
 
-  def _createsChemicalClass(c):
+    def _createsChemicalClass(c):
 
-    _oldSetup = c.setUp
-    _oldTearDown = c.tearDown
+        _oldSetup = c.setUp
+        _oldTearDown = c.tearDown
 
-    chemicalClass = ChemicalClass(label=label, description=description)
+        chemicalClass = ChemicalClass(label=label, description=description)
 
-    def setUp(self):
-      chemicalClass.save()
-      _oldSetup(self)
+        def setUp(self):
+            chemicalClass.save()
+            _oldSetup(self)
 
-    def tearDown(self):
-      chemicalClass.delete()
-      _oldTearDown(self)
+        def tearDown(self):
+            chemicalClass.delete()
+            _oldTearDown(self)
 
-    c.setUp = setUp
-    c.tearDown = tearDown
-    return c
-  return _createsChemicalClass
+        c.setUp = setUp
+        c.tearDown = tearDown
+        return c
+    return _createsChemicalClass
 
 def joinsLabGroup(username, labGroupTitle):
-  '''A class decorator that creates a test lab group with labGroupTitle as it's title and assigns user identified by
-  username to that lab group'''
-  def _joinsLabGroup(c):
-    _oldSetup = c.setUp
-    _oldTearDown = c.tearDown
+    '''A class decorator that creates a test lab group with labGroupTitle as it's title and assigns user identified by
+    username to that lab group'''
+    def _joinsLabGroup(c):
+        _oldSetup = c.setUp
+        _oldTearDown = c.tearDown
 
-    labGroup = LabGroup(title=labGroupTitle, address='War drobe', email='Aslan@example.com', access_code='new_magic')
+        labGroup = LabGroup(title=labGroupTitle, address='War drobe', email='Aslan@example.com', access_code='new_magic')
 
-    def setUp(self):
-      user = User.objects.get(username=username)
-      labGroup.save()
-      user.labgroup_set.add(labGroup)
-      _oldSetup(self)
+        def setUp(self):
+            user = User.objects.get(username=username)
+            labGroup.save()
+            user.labgroup_set.add(labGroup)
+            _oldSetup(self)
 
-    def tearDown(self):
-      _oldTearDown(self)
-      labGroup.delete()
+        def tearDown(self):
+            _oldTearDown(self)
+            labGroup.delete()
 
-    c.setUp = setUp
-    c.tearDown = tearDown
-    return c
-  return _joinsLabGroup
+        c.setUp = setUp
+        c.tearDown = tearDown
+        return c
+    return _joinsLabGroup
 
 def signsExampleLicense(username):
-  '''A class decorator that creates a test license and makes the user specified by username sign it on setUp'''
-  def _signsExampleLicense(c):
+    '''A class decorator that creates a test license and makes the user specified by username sign it on setUp'''
+    def _signsExampleLicense(c):
 
-    _oldSetup = c.setUp
-    _oldTearDown = c.tearDown
+        _oldSetup = c.setUp
+        _oldTearDown = c.tearDown
 
-    license = License(text='This is an example license used in a test', effectiveDate=date.today() - timedelta(1))
+        license = License(text='This is an example license used in a test', effectiveDate=date.today() - timedelta(1))
 
-    def setUp(self):
-      user = User.objects.get(username=username)
-      license.save()
-      self.agreement = LicenseAgreement(user=user, text=license)
-      self.agreement.save()
-      _oldSetup(self)
+        def setUp(self):
+            user = User.objects.get(username=username)
+            license.save()
+            self.agreement = LicenseAgreement(user=user, text=license)
+            self.agreement.save()
+            _oldSetup(self)
 
-    def tearDown(self):
-      self.agreement.delete()
-      license.delete()
-      _oldTearDown(self)
+        def tearDown(self):
+            self.agreement.delete()
+            license.delete()
+            _oldTearDown(self)
 
-    c.setUp = setUp
-    c.tearDown = tearDown
-    return c
-  return _signsExampleLicense
+        c.setUp = setUp
+        c.tearDown = tearDown
+        return c
+    return _signsExampleLicense
 
 def loadsCompoundsFromCsv(labGroupTitle, csvFileName):
-  '''A class decorator that creates a test set of compounds using the csvFileName, which should be stored in the tests directory resource folder.'''
+    '''A class decorator that creates a test set of compounds using the csvFileName, which should be stored in the tests directory resource folder.'''
 
-  def _loadsCompoundsFromCsv(c):
+    def _loadsCompoundsFromCsv(c):
 
-    _oldSetup = c.setUp
-    _oldTearDown = c.tearDown
+        _oldSetup = c.setUp
+        _oldTearDown = c.tearDown
 
-    def setUp(self):
-      labGroup = LabGroup.objects.get(title=labGroupTitle)
-      compounds = labGroup.compound_set.fromCsv(os.path.join(settings.APP_DIR, 'tests', 'resource', csvFileName))
-      for compound in compounds:
-        compound.csConsistencyCheck()
-        compound.save()
-      _oldSetup(self)
+        def setUp(self):
+            labGroup = LabGroup.objects.get(title=labGroupTitle)
+            compounds = labGroup.compound_set.fromCsv(os.path.join(settings.APP_DIR, 'tests', 'resource', csvFileName))
+            for compound in compounds:
+                compound.csConsistencyCheck()
+                compound.save()
+            _oldSetup(self)
 
-    def tearDown(self):
-      Compound.objects.all().delete()
-      _oldTearDown(self)
+        def tearDown(self):
+            Compound.objects.all().delete()
+            _oldTearDown(self)
 
-    c.setUp = setUp
-    c.tearDown = tearDown
+        c.setUp = setUp
+        c.tearDown = tearDown
+        return c
+    return _loadsCompoundsFromCsv
+
+def createsPerformedReactionSet(c):
+# Create a bunch of simple sample reactions.
+    c=createsPerformedReaction("Watchmen", "Rorschach", "R01", ["EtOH"], ["Org"], [0.13],
+                          {"outcome":1, "testNumber":5.04}, duplicateRef='R15')(c)
+    c=createsPerformedReaction("Watchmen", "Rorschach","R02", ["EtOH"], ["Org"], [0.71],
+                          {"outcome":1, "testNumber":5.0})(c)
+    c=createsPerformedReaction("Watchmen", "Rorschach","R03", ["dmed"], ["Org"], [0.1],
+                          {"outcome":1, "testNumber":5.0})(c)
+    c=createsPerformedReaction("Watchmen", "Rorschach","R04", ["dmed"], ["Org"], [0.18],
+                          {"outcome":1, "testNumber":5.0})(c)
+    c=createsPerformedReaction("Watchmen", "Rorschach","R05", ["dabco"], ["Org"], [0.71],
+                          {"outcome":1, "testNumber":5.04})(c)
+    c=createsPerformedReaction("Watchmen", "Rorschach","R06", ["dabco"], ["Org"], [0.14],
+                          {"outcome":1, "testNumber":5.01})(c)
+    c=createsPerformedReaction("Watchmen", "Rorschach","R07", ["Water"], ["Water"], [0.14],
+                          {"outcome":1, "testNumber":5.01})(c)
+    c=createsPerformedReaction("Watchmen", "Rorschach","R08", ["EtOH", "dmed", "Water"],
+                          ["Org", "Org", "Water"],[0.31, 0.3, 0.5],
+                          {"outcome":4, "testNumber":0.1})(c)
+    c=createsPerformedReaction("Watchmen", "Rorschach","R09", ["dmed", "EtOH", "Water"],
+                          ["Org", "Org", "Water"],[0.2, 0.34, 0.3],
+                          {"outcome":4, "testNumber":0.1})(c)
+    c=createsPerformedReaction("Watchmen", "Rorschach","R10", ["dabco", "Water"],
+                          ["Org", "Water"],[0.3, 0.32],
+                          {"outcome":3, "testNumber":0.1})(c)
+    c=createsPerformedReaction("Watchmen", "Rorschach","R11",["Water", "dabco"],
+                          ["Water", "Org"],[0.3, 0.34],
+                          {"outcome":3, "testNumber":0.1})(c)
+    c=createsPerformedReaction("Watchmen", "Rorschach","R12",["dmed", "Water"],
+                          ["Org", "Water"],[0.3, 0.34],
+                          {"outcome":3, "testNumber":0.1})(c)
+    c=createsPerformedReaction("Watchmen", "Rorschach","R13",["EtOH", "Water"],
+                          ["Org", "Water"],[0.3, 0.35],
+                          {"outcome":3, "testNumber":0.1})(c)
+    c=createsPerformedReaction("Watchmen", "Rorschach","R14",["dmed", "Water"],
+                          ["Org", "Water"],[0.4, 0.56],
+                          {"outcome":4, "testNumber":0.01})(c)
+    c=createsPerformedReaction("Watchmen", "Rorschach","R15",["Water", "EtOH"],
+                          ["Water", "Org"],[0.4, 0.57],
+                          {"outcome":4, "testNumber":0.02})(c)
+    c=createsCompoundRole('Org', 'Organic')(c)
+    c=createsCompoundRole('Water', 'Water')(c)
+    c=createsCompound('EtOH', 682, 'Org', 'Watchmen')(c)
+    c=createsCompound('dmed', 67600, 'Org', 'Watchmen')(c)
+    c=createsCompound('dabco', 8882, 'Org', 'Watchmen')(c)
+    c=createsCompound('Water', 937, 'Water', 'Watchmen')(c)
+    c=createsChemicalClass('Org', 'Organic')(c)
+    c=createsChemicalClass('Water', 'Water')(c)
     return c
-  return _loadsCompoundsFromCsv
