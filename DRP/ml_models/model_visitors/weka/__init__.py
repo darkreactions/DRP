9ce4d1ce9e4856b7d31a9666af73070bb3bc0b35
Geoffrey Martin-Noble <gmartinn@haverford.edu>
2016-01-28 00:52:08 -0500

    Added model visitor and test for Naive Bayes

--- DRP/ml_models/model_visitors/weka/__init__.py
+++ DRP/ml_models/model_visitors/weka/__init__.py
@@ -1,5 +1,6 @@
 from SVM_PUK_basic import SVM_PUK_basic
 from SVM_PUK_BCR import SVM_PUK_BCR
 from KNN import KNN
+from NaiveBayes import NaiveBayes
 
-tools = ("SVM_PUK_basic", "SVM_PUK_BCR", "KNN")
+tools = ("SVM_PUK_basic", "SVM_PUK_BCR", "KNN", "NaiveBayes")
