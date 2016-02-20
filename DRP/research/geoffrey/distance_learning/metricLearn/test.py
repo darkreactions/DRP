fae1961520e50cad9e657a698af21f0400a6d106
Geoffrey Martin-Noble <gmartinn@haverford.edu>
2016-02-19 21:10:46 -0500

    added test file because git

--- DRP/research/geoffrey/distance_learning/metricLearn/test.py
+++ DRP/research/geoffrey/distance_learning/metricLearn/test.py
@@ -1,13 +1,12 @@
+from modshogun import LMNN as shogun_LMNN
+from modshogun import RealFeatures, MulticlassLabels
 import numpy as np
-from metric_learn import ITML
+from metric_learn import LMNN
 from sklearn.datasets import load_iris
 
 iris_data = load_iris()
 X = iris_data['data']
 Y = iris_data['target']
 
-itml = ITML()
-
-num_constraints = 200
-C = ITML.prepare_constraints(Y, X.shape[0], num_constraints)
-itml.fit(X, C, verbose=False)
+lmnn = LMNN(k=5, learn_rate=1e-6)
+lmnn.fit(X, Y, verbose=False)
