81eaf1c04430d7abf9dd06cdb3b9621accb8ff2b
Geoffrey Martin-Noble <gmartinn@haverford.edu>
2016-02-21 01:01:02 -0500

    Fixed indents in a couple files

--- DRP/ml_models/splitters/MutualInfoSplitter.py
+++ DRP/ml_models/splitters/MutualInfoSplitter.py
@@ -4,49 +4,49 @@ from django.db.models import Count
 import random
 
 class Splitter(AbstractSplitter):
-  def __init__(self, namingStub):
-    super(Splitter, self).__init__(namingStub)
-    self.TEST_PERCENT = 0.30
-    self.MAX_PARTITION_SIZE = 35 # Magic # TODO: Please de-magic this.
-    self.MIN_TRAIN_SIZE = 10 # WEKA requires at least 10 training points for SVMs.
+    def __init__(self, namingStub):
+        super(Splitter, self).__init__(namingStub)
+        self.TEST_PERCENT = 0.30
+        self.MAX_PARTITION_SIZE = 35 # Magic # TODO: Please de-magic this.
+        self.MIN_TRAIN_SIZE = 10 # WEKA requires at least 10 training points for SVMs.
 
-  def split(self, reactions):
+    def split(self, reactions):
 
-    # Partition the reactions based on what compounds they contain.
-    key_counts = self._count_compound_sets(reactions).items()
-    random.shuffle(key_counts)
+        # Partition the reactions based on what compounds they contain.
+        key_counts = self._count_compound_sets(reactions).items()
+        random.shuffle(key_counts)
 
-    max_test_size = self.TEST_PERCENT*sum(count for key, count in key_counts)
-    test_size = 0
-    train_size = 0
+        max_test_size = self.TEST_PERCENT*sum(count for key, count in key_counts)
+        test_size = 0
+        train_size = 0
 
-    # Determine which partitions should be tested.
-    test_keys = []
-    for key, count in key_counts:
-      if train_size < self.MIN_TRAIN_SIZE:
-        train_size += count
-      elif test_size < max_test_size and count <= self.MAX_PARTITION_SIZE:
-        test_keys.append((key, count))
-        test_size += count
+        # Determine which partitions should be tested.
+        test_keys = []
+        for key, count in key_counts:
+            if train_size < self.MIN_TRAIN_SIZE:
+                train_size += count
+            elif test_size < max_test_size and count <= self.MAX_PARTITION_SIZE:
+                test_keys.append((key, count))
+                test_size += count
 
-    # Get the primary keys of the reactions that should be tested.
-    test_reactions = set()
-    for key, count in test_keys:
-      # Get the reactions that have only the compounds in a given key.
-      partition = reactions.annotate(c=Count('compounds')).filter(c=len(key))
-      for compound in key:
-        partition = partition.filter(compounds=compound)
+        # Get the primary keys of the reactions that should be tested.
+        test_reactions = set()
+        for key, count in test_keys:
+            # Get the reactions that have only the compounds in a given key.
+            partition = reactions.annotate(c=Count('compounds')).filter(c=len(key))
+            for compound in key:
+                partition = partition.filter(compounds=compound)
 
-      test_reactions.update(reaction.pk for reaction in partition)
+            test_reactions.update(reaction.pk for reaction in partition)
 
-    # Actually perform the query.
-    train = reactions.exclude(pk__in=test_reactions)
-    test = reactions.filter(pk__in=test_reactions)
+        # Actually perform the query.
+        train = reactions.exclude(pk__in=test_reactions)
+        test = reactions.filter(pk__in=test_reactions)
 
-    return [(self.package(train), self.package(test))]
+        return [(self.package(train), self.package(test))]
 
-  def _count_compound_sets(self, reactions):
-    compound_sets = [frozenset(Compound.objects.filter(reaction=rxn)) for rxn in reactions]
-    counts = {b: sum(a==b for a in compound_sets) for b in compound_sets}
-    return counts
+    def _count_compound_sets(self, reactions):
+        compound_sets = [frozenset(Compound.objects.filter(reaction=rxn)) for rxn in reactions]
+        counts = {b: sum(a==b for a in compound_sets) for b in compound_sets}
+        return counts
 
