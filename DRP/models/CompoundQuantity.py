d2d27d418933b6af9e803727538287f7d2a265ea
Geoffrey Martin-Noble <gmartinn@haverford.edu>
2016-03-06 19:20:20 -0500

    fixed CompoundQuantityManager inheritance and moved to 4 spaces

--- DRP/models/CompoundQuantity.py
+++ DRP/models/CompoundQuantity.py
@@ -9,47 +9,47 @@ from PerformedReaction import PerformedReaction
 
 class CompoundQuantityQuerySet(models.query.QuerySet):
 
-  def delete(self):
-    reactions = Reaction.objects.filter(compoundquantity_set__in=self)
-    for reaction in reactions:
-      reaction.save() #recalculate descriptors
-      try:
-        reaction.performedreaction.save()
-      except PerformedReaction.DoesNotExist:
-        pass #we don't care about this outcome
+    def delete(self):
+        reactions = Reaction.objects.filter(compoundquantity_set__in=self)
+        for reaction in reactions:
+            reaction.save() #recalculate descriptors
+            try:
+                reaction.performedreaction.save()
+            except PerformedReaction.DoesNotExist:
+                pass #we don't care about this outcome
 
-class CompoundQuantityManager(models.Model):
+class CompoundQuantityManager(models.Manager):
 
-  def get_queryset(self):
-    return CompoundQuantityQuerySet(self.model, using=self._db)
+    def get_queryset(self):
+        return CompoundQuantityQuerySet(self.model, using=self._db)
 
 class CompoundQuantity(models.Model):
-  '''A class to contain the relationship between a reaction and a compound,
-  and thus to contain the amount of a given compound used in a reaction
-  with the applicable units. At present, no unit convention is enforced.
-  '''
+    '''A class to contain the relationship between a reaction and a compound,
+    and thus to contain the amount of a given compound used in a reaction
+    with the applicable units. At present, no unit convention is enforced.
+    '''
 
-  class Meta:
-    app_label='DRP'
-    unique_together=('reaction', 'role', 'amount')
+    class Meta:
+        app_label='DRP'
+        unique_together=('reaction', 'role', 'amount')
 
-  compound=models.ForeignKey(Compound, on_delete=models.PROTECT)
-  reaction=models.ForeignKey(Reaction)
-  role=models.ForeignKey(CompoundRole)
-  amount=models.FloatField(null=True, blank=True)
+    compound=models.ForeignKey(Compound, on_delete=models.PROTECT)
+    reaction=models.ForeignKey(Reaction)
+    role=models.ForeignKey(CompoundRole)
+    amount=models.FloatField(null=True, blank=True)
 
-  def save(self, *args, **kwargs):
-    self.reaction.save() #descriptor recalculation
-    try:
-      self.reaction.performedreaction.save() #invalidate models
-    except PerformedReaction.DoesNotExist:
-      pass #we don't care that it doesn't exist
-    super(CompoundQuantity, self).save()
+    def save(self, *args, **kwargs):
+        self.reaction.save() #descriptor recalculation
+        try:
+            self.reaction.performedreaction.save() #invalidate models
+        except PerformedReaction.DoesNotExist:
+            pass #we don't care that it doesn't exist
+        super(CompoundQuantity, self).save()
 
-  def delete(self):
-    self.reaction.save() #descriptor recalculation
-    try:
-      self.reaction.performedreaction.save() #invalidate models
-    except PerformedReaction.DoesNotExist:
-      pass #we don't care
-    super(CompoundQuantity, self).save()
+    def delete(self):
+        self.reaction.save() #descriptor recalculation
+        try:
+            self.reaction.performedreaction.save() #invalidate models
+        except PerformedReaction.DoesNotExist:
+            pass #we don't care
+        super(CompoundQuantity, self).save()
