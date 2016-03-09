c07ef7283e17dc208354b9dfa33594183164826d
Geoffrey Martin-Noble <gmartinn@haverford.edu>
2016-03-08 22:22:32 -0500

    wrote multiple model building script

--- DRP/research/geoffrey/parse_model_results.py
+++ DRP/research/geoffrey/parse_model_results.py
@@ -2,22 +2,29 @@ import glob
 from sys import argv
 from numpy import mean
 
-def get_val(filepath, name):
-    start_line = name + ':'
+def get_val_from_file(filepath, name):
     with open(filepath, 'rb') as f:
         raw_lines = f.readlines()
-    for line in raw_lines:
+    return get_val(raw_lines, name)
+            
+def get_val(lines, name):
+    start_line = name + ':'
+    for line in lines:
         if line.startswith(start_line):
             val = line.split()[1]
             return val
 
-def get_average_val(filepath, name):
-    start_line = name + ':'
+def get_average_val_from_file(filepath, name):
     with open(filepath, 'rb') as f:
         raw_lines = f.readlines()
+    
+    return get_average_val(raw_lines, name)
+
+def get_average_val(lines, name):
+    start_line = name + ':'
     skipped_first = False
     vals = []
-    for line in raw_lines:
+    for line in lines:
         if line.startswith(start_line):
             if skipped_first:
                 val = float(line.split()[1])
@@ -26,9 +33,8 @@ def get_average_val(filepath, name):
                 skipped_first = True
     return mean(vals)
 
-
-if __name__ == '__main__':
-    directory = 'legacy_tests'
+if __name__ == '__main__':  
+    directory = 'legacy_tests/model_out/'
 
     filepaths = sorted(glob.glob('{}/*.out'.format(directory)))
 
@@ -50,7 +56,8 @@ if __name__ == '__main__':
             metric = fn_split[1]
             fs = fn_split[2]
             mod_string = '{}\t{}\t{}'.format(fs, metric, model)
-
+        else:
+            raise RuntimeError("Don't know how to handle this many parts in a filenmae")
         
         
-        print "{}\t{}\t{}".format(mod_string, get_average_val(fp, 'BCR'), get_average_val(fp, 'Accuracy'))
+        print "{}\t{}\t{}".format(mod_string, get_val_from_file(fp, 'BCR'), get_val_from_file(fp, 'Accuracy'))
