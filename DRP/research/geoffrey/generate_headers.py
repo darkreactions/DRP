7407ab5965bc8dd10d781d3e0d3f24a70e8b2a40
Geoffrey Martin-Noble <gmartinn@haverford.edu>
2016-03-13 18:49:36 -0400

    Wrote code to filter a descriptor set through a set of reactions and only return the ones that have values with variation for the reactions. Extremely conservative feature selection

--- DRP/research/geoffrey/generate_headers.py
+++ DRP/research/geoffrey/generate_headers.py
@@ -2,9 +2,10 @@
 import django
 django.setup()
 from DRP.models import Descriptor
-from DRP.models.rxnDescriptors import BoolRxnDescriptor, OrdRxnDescriptor, NumRxnDescriptor, CatRxnDescriptor
-from DRP.models.predRxnDescriptors import PredBoolRxnDescriptor, PredOrdRxnDescriptor, PredNumRxnDescriptor, PredCatRxnDescriptor
-
+from DRP.models import PredBoolRxnDescriptor, PredOrdRxnDescriptor, PredNumRxnDescriptor, PredCatRxnDescriptor
+from DRP.models import PerformedReaction, DataSet, Descriptor, BoolRxnDescriptor, BoolRxnDescriptorValue, NumRxnDescriptor, NumRxnDescriptorValue, OrdRxnDescriptor, OrdRxnDescriptorValue, CatRxnDescriptor, CatRxnDescriptorValue
+from django.db.models import Count
+from sys import argv
 
 def is_descriptor_type(descriptor_type, descriptor):
     """Returns boolean whether descriptor is of type descriptor_type"""
@@ -40,12 +41,31 @@ def print_descriptor_types():
         print "There are {} descriptors of the {} type".format(number, descType)
 
 
-def print_headers_with_names():
-    for descriptor in valid_descriptors():
+def filter_through_reactions(reactions, descriptors):
+    valid_descriptors = []
+    
+    for descriptor in descriptors:
+        bqs = BoolRxnDescriptorValue.objects.filter(descriptor=descriptor, reaction__in=reactions).exclude(value=None)
+        nqs = NumRxnDescriptorValue.objects.filter(descriptor=descriptor, reaction__in=reactions).exclude(value=None)
+        oqs = OrdRxnDescriptorValue.objects.filter(descriptor=descriptor, reaction__in=reactions).exclude(value=None)
+        cqs = CatRxnDescriptorValue.objects.filter(descriptor=descriptor, reaction__in=reactions).exclude(value=None)
+        if bqs.exists() and len(bqs.values('value').annotate(num=Count('value'))) != 1: # the latter clause checks whether the values are unique
+            valid_descriptors.append(descriptor)
+        if nqs.exists() and len(nqs.values('value').annotate(num=Count('value'))) != 1: # the latter clause checks whether the values are unique
+            valid_descriptors.append(descriptor)
+        if oqs.exists() and len(oqs.values('value').annotate(num=Count('value'))) != 1: # the latter clause checks whether the values are unique
+            valid_descriptors.append(descriptor)
+        if cqs.exists() and len(cqs.values('value').annotate(num=Count('value'))) != 1: # the latter clause checks whether the values are unique
+            valid_descriptors.append(descriptor)
+
+    return valid_descriptors
+
+def print_headers_with_names(descriptors):
+    for descriptor in descriptors:
         print descriptor.heading, '\n\t', descriptor.name, '\n'
 
-def print_headers():
-    for descriptor in valid_descriptors():
+def print_headers(descriptors):
+    for descriptor in descriptors:
         print descriptor.heading
 
 def valid_descriptors():
@@ -67,5 +87,15 @@ def valid_descriptors():
 
 if __name__=='__main__':
     #print_headers()
-    print_headers_with_names()
+    #print_headers_with_names()
     #print_descriptor_types()
+    in_file = argv[1]
+    reaction_set_name = argv[2]
+
+    with open(in_file) as f:
+        descriptor_headers = [l.strip() for l in f.readlines()]
+
+    descriptors = Descriptor.objects.filter(heading__in=descriptor_headers)
+    reactions = DataSet.objects.get(name=reaction_set_name).reactions.all()
+
+    print_headers(filter_through_reactions(reactions, descriptors))
