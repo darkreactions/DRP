693d64b0cdda8e35e8525975e6117c3df096f54f
Geoffrey Martin-Noble <gmartinn@haverford.edu>
2016-03-14 12:43:16 -0400

    parsed models and moved

--- DRP/research/geoffrey/launch_all_models.sh
+++ DRP/research/geoffrey/launch_all_models.sh
@@ -1,13 +1,13 @@
 SCRIPT="../build_model.py"
-DESCRIPTOR_FILE="descs/legacy_valid_allNewReplacements.dsc"
+DESCRIPTOR_FILE="descs/full_valid.dsc"
 OUTCOME_DESCRIPTOR="boolean_outcome_legacy"
 RXN_SET_NAME="valid_legacy_reactions"
-COMMENT="Legacy descriptors, all new replacements. 15 mutual info splits."
-FILE_SUFFIX="legRxnNonZeroCompound_legDscValidAllNewReplacements"
+COMMENT="New Descriptors with NonZero Variance. 15 mutual info splits."
+FILE_SUFFIX="legRxnNonZeroCompound_newDscNonZeroVar"
 SPLITTER="MutualInfoSplitter"
 
 python -u $SCRIPT -p @$DESCRIPTOR_FILE -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -v -mt SVM_PUK_basic -d "SVM $COMMENT" &> "SVMbasic_$FILE_SUFFIX.out" &
 python -u $SCRIPT -p @$DESCRIPTOR_FILE -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -v -mt SVM_PUK_BCR -d "$BCR SVM $COMMENT" &> "SVMBCR_$FILE_SUFFIX.out" &
 python -u $SCRIPT -p @$DESCRIPTOR_FILE -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -v -mt J48 -d "J48 $COMMENT" &> "J48_$FILE_SUFFIX.out" &
 python -u $SCRIPT -p @$DESCRIPTOR_FILE -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -v -mt KNN -d "KNN $COMMENT" &> "KNN_$FILE_SUFFIX.out" &
-python -u $SCRIPT -p @$DESCRIPTOR_FILE -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -v -mt NaiveBayes -d "NB $COMMENT" &> "NB_$FILE_SUFFIX.out" && source ../launch_all_models2.sh &
+python -u $SCRIPT -p @$DESCRIPTOR_FILE -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -v -mt NaiveBayes -d "NB $COMMENT" &> "NB_$FILE_SUFFIX.out" &
--- DRP/research/geoffrey/legacy_tests/run_things.py
+++ DRP/research/geoffrey/legacy_tests/run_things.py
@@ -1,20 +1,7 @@
 import django
 django.setup()
-from DRP.models import PerformedReaction, DataSet
+from DRP.models import PerformedReaction, DataSet, Descriptor
 from django.db.models import Count
 
-reactions = DataSet.objects.get(name='legacy_reactions').reactions.all()
-#reactions = PerformedReaction.objects.all()
-#reactions = reactions.filter(valid=True)
-
-reactions = reactions.annotate(compound_count=Count('compounds'))
-with open('legacy_reactions_allDescs.csv', 'wb') as f:
-    reactions.toCsv(f, expanded=True)
-
-#compound_numbers = [reactions.filter(compound_count=i).count() for i in xrange(10)]
-#print compound_numbers
-
-
-#rxn0compound = reactions.filter(compound_count=0)
-#for rxn in rxn0compound:
-    #print rxn
+for d in Descriptor.objects.all():
+    print d.heading
