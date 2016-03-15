e3af6bc43c1762d7a130f32ca333849654cfed18
gmartinn <gmartinn@haverford.edu>
2016-03-15 17:37:37 -0400

    built more models. Fixed model building launch shell scripts. renamed some things

--- DRP/research/geoffrey/launch_all_models.sh
+++ DRP/research/geoffrey/launch_all_models.sh
@@ -1,13 +1,13 @@
 SCRIPT="../build_model.py"
-DESCRIPTOR_FILE="descs/full_valid.dsc"
+DESCRIPTOR_FILE="descs/legacy_new_nonZeroVariance.dsc"
 OUTCOME_DESCRIPTOR="boolean_outcome_legacy"
 RXN_SET_NAME="valid_legacy_reactions"
-COMMENT="New Descriptors with NonZero Variance. 15 mutual info splits."
-FILE_SUFFIX="legRxnNonZeroCompound_newDscNonZeroVar"
+COMMENT="Legacy and New Descriptors with NonZero Variance. 15 mutual info splits."
+FILE_SUFFIX="legRxnNonZeroCompound_legNewDscNonZeroVar"
 SPLITTER="MutualInfoSplitter"
 
 python -u $SCRIPT -p @$DESCRIPTOR_FILE -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -v -mt SVM_PUK_basic -d "SVM $COMMENT" &> "SVMbasic_$FILE_SUFFIX.out" &
-python -u $SCRIPT -p @$DESCRIPTOR_FILE -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -v -mt SVM_PUK_BCR -d "$BCR SVM $COMMENT" &> "SVMBCR_$FILE_SUFFIX.out" &
+python -u $SCRIPT -p @$DESCRIPTOR_FILE -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -v -mt SVM_PUK_BCR -d "BCR SVM $COMMENT" &> "SVMBCR_$FILE_SUFFIX.out" &
 python -u $SCRIPT -p @$DESCRIPTOR_FILE -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -v -mt J48 -d "J48 $COMMENT" &> "J48_$FILE_SUFFIX.out" &
 python -u $SCRIPT -p @$DESCRIPTOR_FILE -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -v -mt KNN -d "KNN $COMMENT" &> "KNN_$FILE_SUFFIX.out" &
 python -u $SCRIPT -p @$DESCRIPTOR_FILE -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -v -mt NaiveBayes -d "NB $COMMENT" &> "NB_$FILE_SUFFIX.out" &
