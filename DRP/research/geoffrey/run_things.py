390469b85a424e975987bd518fd88e524736753d
gmartinn <gmartinn@haverford.edu>
2016-03-19 18:10:27 -0400

    commit run_things for pull

--- DRP/research/geoffrey/run_things.py
+++ DRP/research/geoffrey/run_things.py
@@ -7,31 +7,16 @@ compounds = Compound.objects.all()
 
 restart = 0
 
-#for i, c in enumerate(compounds):
-    #if i >= restart:
-        #print u"Calculating for compound {}/{}: {}".format(i+1, compounds.count(), c)
-        #try:
-            #c.save(calcDescriptors=True, invalidateReactions=False)
-        #except ValueError as e:
-            #print e
-
-#for d in Descriptor.objects.all():
-    #if d.calculatorSoftware != "DRP":
-        #print d.heading, d.calculatorSoftware
-
-
+for i, c in enumerate(compounds):
+    if i >= restart:
+        print u"Calculating for compound {}/{}: {}".format(i+1, compounds.count(), c)
+        try:
+            c.save(calcDescriptors=True, invalidateReactions=False)
+        except ValueError as e:
+            print e
 
 rxns = PerformedReaction.objects.all()
 
 rxns = rxns.filter(valid=True).exclude(compounds=None)
 
 rxns.calculate_descriptors(verbose=True)
-
-#rxns.calculate_descriptors(verbose=True)
-
-#for i, r in enumerate(rxns):
-    #if i > 1:
-        #break
-    #print "Calculating for reaction {}/{}: {}".format(i+1, rxns.count(), r)
-    #r.save()
-
