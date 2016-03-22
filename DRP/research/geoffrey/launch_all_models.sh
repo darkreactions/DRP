407da2e543e1d55acc546b517c2bdd266d16f5d0
gmartinn <gmartinn@haverford.edu>
2016-03-22 00:49:42 -0400

    redid descriptor files with manual descriptors back in. Moved and renamed bad model files

--- DRP/research/geoffrey/launch_all_models.sh
+++ DRP/research/geoffrey/launch_all_models.sh
@@ -2,11 +2,11 @@ SCRIPT="../build_model.py"
 DESCRIPTOR_FILE="descs/new_cxcalc_nonZeroVariance.dsc"
 OUTCOME_DESCRIPTOR="boolean_outcome_legacy"
 RXN_SET_NAME="valid_legacy_rxns_nonzero_compound"
-COMMENT="Legacy rxns. New descriptors including cxcalc. 15 mutual info split."
-FILE_SUFFIX="legRxnNonZeroCompound_NewCXDscNonZeroVar_15MISplit"
+COMMENT="Legacy rxns. New descriptors including cxcalc nonzero variance. 15 mutual info split."
+FILE_SUFFIX="legRxnNonZeroCompound_NewCXDscNonZeroVar"
 SPLITTER="MutualInfoSplitter"
 
-python -u $SCRIPT -p @$DESCRIPTOR_FILE -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -v -mt SVM_PUK_basic -d "SVM $COMMENT" &> "SVMbasic_$FILE_SUFFIX.out" &
+python -u $SCRIPT -p @$DESCRIPTOR_FILE -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -v -mt SVM_PUK_basic -d "SVM $COMMENT" &> "SVMbasic_$FILE_SUFFIX.out"
 python -u $SCRIPT -p @$DESCRIPTOR_FILE -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -v -mt SVM_PUK_BCR -d "BCR SVM $COMMENT" &> "SVMBCR_$FILE_SUFFIX.out" &
 python -u $SCRIPT -p @$DESCRIPTOR_FILE -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -v -mt J48 -d "J48 $COMMENT" &> "J48_$FILE_SUFFIX.out" &
 python -u $SCRIPT -p @$DESCRIPTOR_FILE -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -v -mt KNN -d "KNN $COMMENT" &> "KNN_$FILE_SUFFIX.out" &
--- DRP/research/geoffrey/launch_all_models2.sh
+++ DRP/research/geoffrey/launch_all_models2.sh
@@ -1,13 +1,13 @@
 SCRIPT="../build_model.py"
-DESCRIPTOR_FILE="descs/legacy_valid_allNewReplacements_removeGroupPeriodValenceSlowCool.dsc"
+DESCRIPTOR_FILE="descs/new_cxcalc_nonZeroVariance.dsc"
 OUTCOME_DESCRIPTOR="boolean_outcome_legacy"
-RXN_SET_NAME="valid_legacy_reactions"
-COMMENT="Legacy descriptors, all new direct replacements - Group, Period, Valence, Slow Cool. 15 mutual info splits."
-FILE_SUFFIX="legRxnNonZeroCompound_legDscValidAllNewReplacementsRemoveGroupPeriodValenceSlowCool"
+RXN_SET_NAME="valid_legacy_rxns_nonzero_compound"
+COMMENT="Legacy rxns. New descriptors including cxcalc. 15 mutual info split."
+FILE_SUFFIX="legRxnNonZeroCompound_NewCXDscNonZeroVar"
 SPLITTER="MutualInfoSplitter"
 
-python -u $SCRIPT -p @$DESCRIPTOR_FILE -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -v -mt SVM_PUK_basic -d "SVM $COMMENT" &> "SVMbasic_$FILE_SUFFIX.out" &
-python -u $SCRIPT -p @$DESCRIPTOR_FILE -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -v -mt SVM_PUK_BCR -d "BCR SVM $COMMENT" &> "SVMBCR_$FILE_SUFFIX.out" &
+#python -u $SCRIPT -p @$DESCRIPTOR_FILE -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -v -mt SVM_PUK_basic -d "SVM $COMMENT" &> "SVMbasic_$FILE_SUFFIX.out" &
+#python -u $SCRIPT -p @$DESCRIPTOR_FILE -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -v -mt SVM_PUK_BCR -d "BCR SVM $COMMENT" &> "SVMBCR_$FILE_SUFFIX.out" &
 python -u $SCRIPT -p @$DESCRIPTOR_FILE -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -v -mt J48 -d "J48 $COMMENT" &> "J48_$FILE_SUFFIX.out" &
-python -u $SCRIPT -p @$DESCRIPTOR_FILE -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -v -mt KNN -d "KNN $COMMENT" &> "KNN_$FILE_SUFFIX.out" &
-python -u $SCRIPT -p @$DESCRIPTOR_FILE -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -v -mt NaiveBayes -d "NB $COMMENT" &> "NB_$FILE_SUFFIX.out" && source ../launch_all_models3.sh &
+#python -u $SCRIPT -p @$DESCRIPTOR_FILE -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -v -mt KNN -d "KNN $COMMENT" &> "KNN_$FILE_SUFFIX.out" &
+#python -u $SCRIPT -p @$DESCRIPTOR_FILE -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -v -mt NaiveBayes -d "NB $COMMENT" &> "NB_$FILE_SUFFIX.out" &
