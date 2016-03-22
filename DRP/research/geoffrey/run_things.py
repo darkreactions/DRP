d5395a6c703f5054e44dd2c87a8703e63125457c
Geoffrey Martin-Noble <gmartinn@haverford.edu>
2016-03-21 21:40:40 -0400

    dumped compound data to csv

--- DRP/research/geoffrey/run_things.py
+++ DRP/research/geoffrey/run_things.py
@@ -3,51 +3,6 @@ django.setup()
 from DRP.models import PerformedReaction, DataSet, Descriptor, Compound, Reaction, CompoundQuantity, NumRxnDescriptorValue, CatMolDescriptorValue, OrdMolDescriptorValue, BoolMolDescriptorValue, NumMolDescriptorValue, CatMolDescriptor, OrdMolDescriptor, BoolMolDescriptor, NumMolDescriptor, BoolRxnDescriptorValue
 from django.db.models import Count
 
-#compounds = Compound.objects.all()
-###restart = 0
-
-#compounds.calculate_descriptors(verbose=True)
-
-#bad_compounds = []
-
-#for i, c in enumerate(compounds):
-    #if i+1 >= restart:
-        #print u"Calculating for compound {}/{}: {}".format(i+1, compounds.count(), c)
-        ##try:
-        #c.save(calcDescriptors=True, invalidateReactions=False)
-        ##except ValueError as e:
-            ##bad_compounds.append(c)
-            ##print e
-
-#print "These compounds were not found: "
-#for c in bad_compounds:
-    #try:
-        #print c
-    #except UnicodeEncodeError as e:
-        #print "Error on compound with abbreviation: ", c.abbrev
-        #print e
-
-# restart_reference = "2tv49.3"
-
 rxns = PerformedReaction.objects.all()
-rxns = rxns.filter(valid=True).exclude(compounds=None)
-rxns = rxns.filter(boolrxndescriptorvalue__in=BoolRxnDescriptorValue.objects.filter(descriptor__heading='boolean_outcome_legacy'))
-
-# min_pk = rxns.get(reference=restart_reference)
-# rxns = rxns.filter(pk__gt=min_pk)
-# rxns.calculate_descriptors(verbose=True)
-
-DataSet.create('valid_legacy_rxns_nonzero_compound', rxns)
-
-#print NumRxnDescriptorValue.objects.exclude(value=None).count()
-
-#for cmd in CatMolDescriptor.objects.all().order_by('heading'):
-    #all_vals = CatMolDescriptorValue.objects.filter(descriptor=cmd)
-    #print cmd, cmd.calculatorSoftware, all_vals.count(), all_vals.exclude(value=None).count()
-#for nmd in NumMolDescriptor.objects.all().order_by('heading'):
-    #all_vals = NumMolDescriptorValue.objects.filter(descriptor=nmd)
-    #print nmd, nmd.calculatorSoftware, all_vals.count(), all_vals.exclude(value=None).count()
-#for omd in OrdMolDescriptor.objects.all().order_by('heading'):
-    #print omd, omd.calculatorSoftware, OrdMolDescriptorValue.objects.filter(descriptor=omd).exclude(value=None).count()
-#for bmd in BoolMolDescriptor.objects.all().order_by('heading'):
-    #print bmd, bmd.calculatorSoftware, BoolMolDescriptorValue.objects.filter(descriptor=bmd).exclude(value=None).count()
+with open('reactions_old.csv', 'wb') as f:
+    rxns.toCsv(f, expanded=True)
