e94a4627f2356b8f4721f57a99b9fc85648e5d82
Geoffrey Martin-Noble <gmartinn@haverford.edu>
2016-03-21 21:46:07 -0400

    stuff

--- DRP/research/geoffrey/run_things.py
+++ DRP/research/geoffrey/run_things.py
@@ -3,6 +3,17 @@ django.setup()
 from DRP.models import PerformedReaction, DataSet, Descriptor, Compound, Reaction, CompoundQuantity, NumRxnDescriptorValue, CatMolDescriptorValue, OrdMolDescriptorValue, BoolMolDescriptorValue, NumMolDescriptorValue, CatMolDescriptor, OrdMolDescriptor, BoolMolDescriptor, NumMolDescriptor, BoolRxnDescriptorValue
 from django.db.models import Count
 
+import argparse
+
+parser = argparse.ArgumentParser(description='dumps to csv', fromfile_prefix_chars='@',
+                                 epilog="Prefix arguments with '@' to specify a file containing newline"
+                                 "-separated values for that argument. e.g.'-p @predictor_headers.txt'"
+                                 " to pass multiple descriptors from a file as predictors")
+
+parser.add_argument('-p', '--predictor-headers', nargs='+',
+                    help='One or more descriptors to use as predictors.', required=True)
+args = parser.parseargs()
+
 rxns = PerformedReaction.objects.all()
 with open('reactions_old.csv', 'wb') as f:
-    rxns.toCsv(f, expanded=True)
+    rxns.toCsv(f, expanded=True, whitelistHeaders=args.predictor_headers)
