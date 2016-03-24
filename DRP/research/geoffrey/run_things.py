b64b4145bd5178e93f8fda72f62cfe015892ae99
Geoffrey Martin-Noble <gmartinn@haverford.edu>
2016-03-23 23:15:41 -0400

    plethora

--- DRP/research/geoffrey/run_things.py
+++ DRP/research/geoffrey/run_things.py
@@ -4,16 +4,18 @@ from DRP.models import PerformedReaction, DataSet, Descriptor, Compound, Reactio
 from django.db.models import Count
 import argparse
 
-if __name__ == '__main__':
-    django.setup()
-    parser = argparse.ArgumentParser(description='Builds a model', fromfile_prefix_chars='@',
-                                     epilog="Prefix arguments with '@' to specify a file containing newline"
-                                     "-separated values for that argument. e.g.'-p @predictor_headers.txt'"
-                                     " to pass multiple descriptors from a file as predictors")
-    parser.add_argument('-d', '--descriptor-headers', nargs='+',
-                        help='One or more descriptors to use as predictors.', required=True)
-    parser.add_argument('-rxn', '--reaction-set-name', default=None,
-                        help='The name of the reactions to use as a whole dataset')
-    args = parser.parse_args()
+print PerformedReaction.objects.all().expandedArffHeaders
 
-    print args.descriptor_headers
+#if __name__ == '__main__':
+    #django.setup()
+    #parser = argparse.ArgumentParser(description='Builds a model', fromfile_prefix_chars='@',
+                                     #epilog="Prefix arguments with '@' to specify a file containing newline"
+                                     #"-separated values for that argument. e.g.'-p @predictor_headers.txt'"
+                                     #" to pass multiple descriptors from a file as predictors")
+    #parser.add_argument('-d', '--descriptor-headers', nargs='+',
+                        #help='One or more descriptors to use as predictors.', required=True)
+    #parser.add_argument('-rxn', '--reaction-set-name', default=None,
+                        #help='The name of the reactions to use as a whole dataset')
+    #args = parser.parse_args()
+
+    #print args.descriptor_headers
