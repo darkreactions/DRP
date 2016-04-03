748d94583c93113759e52d6f0ca136629ac36dab
gmartinn <gmartinn@haverford.edu>
2016-04-03 01:58:16 -0400

    Figured out absurd extra newline error >:-(

--- DRP/research/geoffrey/launch_many_models.sh
+++ DRP/research/geoffrey/launch_many_models.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 SCRIPT="../build_many_models.py"
-DESCRIPTOR_FILE="descs/new_noCA_nonZeroInfo.dsc"
-OUTCOME_DESCRIPTOR="boolean_crystallisation_outcome" #"boolean_outcome_legacy"
-RXN_SET_NAME="Testing_0"
-COMMENT="TEST" #Legacy rxns. Legacy descriptors with ChemAxon nonzero InfoGain. 15 mutual info split."
-FILE_SUFFIX="TEST" #"legRxnNonZeroCompound_LegDscCANonzeroInfo_15MISplit"
+DESCRIPTOR_FILE="descs/legacy_noCA_nonZeroInfo.dsc"
+OUTCOME_DESCRIPTOR="boolean_outcome_legacy"
+RXN_SET_NAME="valid_legacy_rxns_nonzero_compound"
+COMMENT="Legacy rxns. Legacy descriptors without ChemAxon nonzero InfoGain. 15 mutual info split."
+FILE_SUFFIX="legRxnNonZeroCompound_LegNoCANonzeroInfo_15MISplit"
 SPLITTER="MutualInfoSplitter"
 SPLITTER_OPTIONS="{'num_splits': 15}"
 MODEL_VISITOR_FILE="mv.txt"
 MODEL_VISITOR_OPTION_FILE="vo.txt"
 
-python -u -m cProfile -o "$FILE_SUFFIX.profile" $SCRIPT -p @$DESCRIPTOR_FILE -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -so "$SPLITTER_OPTIONS" -v -mt @"$MODEL_VISITOR_FILE" -vo @"$MODEL_VISITOR_OPTION_FILE" -d "$COMMENT"
+python -u -m cProfile -o "$FILE_SUFFIX.profile" $SCRIPT -p @"$DESCRIPTOR_FILE" -rxn $RXN_SET_NAME -r $OUTCOME_DESCRIPTOR -s $SPLITTER -so "$SPLITTER_OPTIONS" -v -mt @"$MODEL_VISITOR_FILE" -vo @"$MODEL_VISITOR_OPTION_FILE" -d "$COMMENT" &> "$FILE_SUFFIX.out" &
