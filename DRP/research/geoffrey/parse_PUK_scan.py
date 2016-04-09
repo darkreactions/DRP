8fce15687d8eb197fec268283db6db0f00179c84
Geoffrey Martin-Noble <gmartinn@haverford.edu>
2016-04-08 22:28:09 -0400

    wrote script for parsing descriptor headers into their underlying calculation. Wrote script for parsing PUK scan results

---
+++ DRP/research/geoffrey/parse_PUK_scan.py
@@ -0,0 +1,45 @@
+import re
+from sys import argv
+import csv
+
+def extract_results(fn, statistic='BCR'):
+    result_line = r"^sigma=([0-9.]+) omega=([0-9.]+) Average {}: ([0-9.]+)".format(statistic)
+    results = []
+    with open(fn) as f:
+        for line in f:
+            res = re.findall(result_line, line)
+            if res:
+                results.append(map(float, res[0]))
+
+    return results
+
+def extract_and_write(fn, csv_fn, statistic):
+    results = extract_results(fn, statistic)
+    
+    sigmas = sorted(set([res[0] for res in results]))
+    omegas = sorted(set([res[0] for res in results]))
+
+    results_dict = {sigma:{} for sigma in sigmas}
+
+    for res in results:
+        sigma, omega, stat = res
+        results_dict[sigma][omega] = stat
+
+    with open(csv_fn, 'w') as f:
+        writer = csv.DictWriter(f, ['sigma'] + omegas)
+        writer.writeheader()
+        for sigma in sigmas:
+            row = {'sigma': sigma}
+            row.update(results_dict[sigma])
+            writer.writerow(row)
+
+if __name__ == '__main__':
+    fn = argv[1]
+    csv_fn = argv[2]
+    try:
+        statistic = argv[3]
+    except IndexError:
+        statistic = 'BCR'
+        
+
+
---
+++ DRP/research/geoffrey/parse_descriptors.py
@@ -0,0 +1,122 @@
+"""Script for parsing descriptor headings into their underlying calculation"""
+from sys import argv
+from DRP import chemical_data
+import csv
+
+def strip_headers(fn, legacy=True):
+    with open(fn) as f:
+        headers = [l.strip() for l in f]
+
+    if legacy:
+        strip_method = legacy_strip
+    else:
+        strip_method = new_strip
+
+    headers = [(header, compress(strip_method(header)).lower()) for header in headers]
+
+    return headers
+    
+def strip_header(header, banned_substrings=[], banned_prefixes=[], banned_suffixes=[]):
+    stripped_header = header
+
+    for pref in banned_prefixes:
+        if stripped_header.startswith(pref):
+            stripped_header = stripped_header[len(pref):]
+            
+    for suf in banned_suffixes:
+        if stripped_header.endswith(suf):
+            stripped_header = stripped_header[:-len(suf)]
+
+    for sub in banned_substrings:
+        stripped_header = stripped_header.replace(sub, '_')
+
+    stripped_header = stripped_header.replace('_', '')
+    #stripped_header = stripped_header.lower()
+
+    return stripped_header
+
+def compress(header):
+    return compress_elements(compress_groups(compress_periods(compress_valence(header))))
+
+def compress_elements(header):
+    return 'element' if header in chemical_data.elements.keys() else header
+
+def compress_groups(header):
+    return 'group' if header in ['G{}'.format(i) for i in range(1,19)] else header
+    
+def compress_periods(header):
+    return 'period' if header in ['P{}'.format(i) for i in range(1,8)] else header
+    
+def compress_valence(header):
+    return 'valence' if header in ['V{}'.format(i) for i in range(1,8)] else header
+
+def legacy_strip(header):
+    banned_substrings = ['Min',
+                        'Max',
+                        'Mean',
+                        'Avg',
+                        'Arith',
+                        'Geom',
+                        '_pHdependent',
+                        'Weighted',
+                        ]
+
+    banned_prefixes = ['oxlike',
+                        'org',
+                        'inorg',
+                        ]
+                    
+    banned_suffixes = ['_legacy',
+                        ]
+
+    return strip_header(header, banned_substrings, banned_prefixes, banned_suffixes)
+
+def new_strip(header):
+    banned_substrings = ['chemaxoncxcalc_15.6',
+                         'drprdkit_0',
+                         'drp_0.02',
+                         'gmean',
+                         'Max',
+                         'Range',
+                         'geom',
+                         'stoich',
+                         'drpInorgAtom',
+                         'unw',
+                         'max_',
+                        ]
+    banned_prefixes = ['Inorg',
+                       'Org',
+                       'Ox',
+                       'Solv',
+                       'reaction',
+                      ]
+    banned_suffixes = ['count',
+                       'molarity',
+                       'mols',
+                       ]
+
+    pH_strings = ['_pH{}_'.format(i) for i in range(1,15)]
+    banned_substrings += pH_strings
+
+    return strip_header(header, banned_substrings, banned_prefixes, banned_suffixes)
+
+if __name__ == '__main__':
+    fn = argv[1]
+    csv_out = argv[2]
+
+    header_tuples = strip_headers(fn, legacy=False)
+
+    #print '\n'.join(['\t'.join(header_tuple) for header_tuple in header_tuples])
+
+    header_dict = {}
+
+    for header, stripped_header in header_tuples:
+        try:
+            header_dict[stripped_header].append(header)
+        except KeyError:
+            header_dict[stripped_header] = [header]
+
+    with open(csv_out, 'w') as f:
+        writer = csv.writer(f)
+        for stripped_header, headers in sorted(header_dict.items()):
+            writer.writerow([stripped_header] + headers)
