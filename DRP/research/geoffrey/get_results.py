aa9fc121ef99af459437a85c93917dc2b06f4ab6
Geoffrey Martin-Noble <gmartinn@haverford.edu>
2016-04-13 17:05:10 -0400

    added distinct to Count aggregations that fixed everything

--- DRP/research/geoffrey/get_results.py
+++ DRP/research/geoffrey/get_results.py
@@ -1,46 +1,44 @@
 #!/usr/bin/env python
 import django
 django.setup()
-from DRP.models import ModelContainer, NumRxnDescriptor
+from DRP.models import ModelContainer, NumRxnDescriptor, BoolRxnDescriptor
 from sys import argv
 import os
 from django.db.models import Count
 
 descriptor_directory = 'descs'
 
-desc_files = ['small.dsc']
+desc_files = ['small_with_bool.dsc']
 splitter = 'MutualInfoSplitter'
 splitterOptions = '"num_splits": 15'
 modelVisitorOptions = ['"BCR": true', '"BCR": false']
 modelVisitorTools = ['J48', 'KNN', 'LogisticRegression', 'NaiveBayes', 'RandomForest', 'SVM_PUK']
 
-containers = ModelContainer.objects.filter(built=True)
+containers = ModelContainer.objects.filter(built=True).annotate(num_numDescs=Count('numRxnDescriptors', distinct=True)).annotate(num_boolDescs=Count('boolRxnDescriptors', distinct=True))
 
-containers = containers.filter(splitterOptions__contains=splitterOptions)
+#containers = containers.filter(splitterOptions__contains=splitterOptions)
 
 for descriptor_file in desc_files:
     descriptor_file_path = os.path.join(descriptor_directory, descriptor_file)
     with open(descriptor_file_path) as f:
         headers = [l.strip() for l in f if l.strip()]
 
+    boolDescs = BoolRxnDescriptor.objects.filter(heading__in=headers)
     numDescs = NumRxnDescriptor.objects.filter(heading__in=headers)
-    num_descs = numDescs.count()
+    num_numDescs = numDescs.count()
+    num_boolDescs = boolDescs.count()
 
-    if numDescs.count() != len(headers):
+    if num_numDescs + num_boolDescs != len(headers):
         raise RuntimeError("Did not find correct number of descriptors")
 
-    #print numDescs
     conts = containers
+    if numDescs:
+        conts = conts.filter(numRxnDescriptors__in=numDescs).distinct()
+    if boolDescs:
+        conts = conts.filter(boolRxnDescriptors__in=boolDescs).distinct()
 
-    conts = conts.filter(numRxnDescriptors__in=numDescs).distinct()
-    #print conts.count()
+    conts = conts.filter(num_numDescs=num_numDescs).filter(num_boolDescs=num_boolDescs)
 
-    conts = conts.annotate(num_descs=Count('numRxnDescriptors'))
-    #for cont in conts:
-        #print cont.num_descs, cont.numRxnDescriptors.all()
-    conts = conts.filter(num_descs=num_descs)
-    #print conts.count()
-    
     for tool in modelVisitorTools:
         model_conts = conts.filter(modelVisitorTool=tool)
         print model_conts.count()
