d5ef065467c6dab4097808b0394fe094082dc850
Geoffrey Martin-Noble <gmartinn@haverford.edu>
2016-05-15 22:44:56 -0400

    added molecular descriptor calculations for group and period

--- DRP/plugins/moldescriptors/drp.py
+++ DRP/plugins/moldescriptors/drp.py
@@ -55,6 +55,22 @@ for prop in inorgAtomicProperties:
         'minimum':None
         }
 
+for group_num in range(1,18):
+    _descriptorDict['boolean_group_{}'.format(group_num)] = {
+        'type': 'bool',
+        'name': 'Presence of elements in group {}'.format(group_num),
+        'calculatorSoftware':'DRP',
+        'calculatorSoftwareVersion':'1.0',
+        }
+        
+for period_num in range(1,7):
+    _descriptorDict['boolean_period_{}'.format(period_num)] = {
+        'type': 'bool',
+        'name': 'Presence of elements in period {}'.format(period_num),
+        'calculatorSoftware':'DRP',
+        'calculatorSoftwareVersion':'1.0',
+        }
+
 descriptorDict = setup(_descriptorDict)
 
 def delete_descriptors(compound_set):
@@ -81,31 +97,48 @@ def calculate(compound):
 def _calculate(compound):
 
     num = DRP.models.NumMolDescriptorValue
+    boolVal = DRP.models.BoolMolDescriptorValue
+
+    num_vals_to_create = []
+    bool_vals_to_create = []
 
     if any(element in inorgElements for element in compound.elements):
-        delete_descriptors([compound])
-        vals_to_create = []
+
         inorgElementNormalisationFactor = sum(info['stoichiometry'] for element, info in compound.elements.items() if element in inorgElements)
         for prop in inorgAtomicProperties:
-            vals_to_create.append(num( 
+            num_vals_to_create.append(num( 
                                         compound=compound,
                                         descriptor=descriptorDict['drpInorgAtom{}_geom_unw'.format(prop.title().replace('_', ''))],
                                         value=gmean([inorgElements[element][prop] for element in compound.elements if element in inorgElements])
                                         ))
 
-            vals_to_create.append(num(
+            num_vals_to_create.append(num(
                                         compound=compound,
                                         descriptor=descriptorDict['drpInorgAtom{}_geom_stoich'.format(prop.title().replace('_', ''))],
                                         value = gmean([inorgElements[element][prop]*(info['stoichiometry']/inorgElementNormalisationFactor) for element, info in compound.elements.items() if element in inorgElements])))
                
-            vals_to_create.append(num(
+            num_vals_to_create.append(num(
                                         compound=compound,
                                         descriptor=descriptorDict['drpInorgAtom{}_max'.format(prop.title().replace('_', ''))],
                                         value = max(inorgElements[element][prop] for element in compound.elements if element in inorgElements)))
     
-            vals_to_create.append(num(
+            num_vals_to_create.append(num(
                                         compound=compound,
                                         descriptor=descriptorDict['drpInorgAtom{}_range'.format(prop.title().replace('_', ''))],
                                         value = max(inorgElements[element][prop] for element in compound.elements if element in inorgElements) - min(inorgElements[element][prop] for element in compound.elements if element in inorgElements)))
-        num.objects.bulk_create(vals_to_create)
-    
+        num.objects.bulk_create(num_vals_to_create)
+
+    for group_num in range(1,18):
+        bool_vals_to_create.append(boolVal(
+                                        compound=compound,
+                                        descriptor=descriptorDict['boolean_group_{}'.format(group_num)],
+                                        value=(any(info['group']==group_num for element, info in compound.elements.items()))
+                                    ))
+    for period_num in range(1,7):
+        bool_vals_to_create.append(boolVal(
+                                        compound=compound,
+                                        descriptor=descriptorDict['boolean_period_{}'.format(period_num)],
+                                        value=(any(info['period']==period_num for element, info in compound.elements.items()))
+                                    ))
+                                    
+    boolVal.bulk_create(bool_vals_to_create)
