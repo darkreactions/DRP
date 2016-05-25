f7eea391dffdf596dd3bcb8e9119517d4c3fb393
Geoffrey Martin-Noble <gmartinn@haverford.edu>
2016-05-25 16:48:24 -0700

    made script turn off model invalidation

--- DRP/research/geoffrey/run_things.py
+++ DRP/research/geoffrey/run_things.py
@@ -9,6 +9,6 @@ for cq in CompoundQuantity.objects.filter(amount=0):
     print rxn
     rxn.valid = False
     rxn.notes += 'Compound {} had amount {}. Amount changed None and reaction invalidated.'.format(cq.compound, cq.amount)
-    rxn.save(calcDescriptors=False)
+    rxn.save(calcDescriptors=False, invalidate_models=False)
     cq.amount = None
-    cq.save(calcDescriptors=False)
+    cq.save(calcDescriptors=False, invalidate_models=False)
