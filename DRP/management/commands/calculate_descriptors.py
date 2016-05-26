df14c7931cc48a1345b8ce8ad9ae119f2b5db791
Geoffrey Martin-Noble <gmartinn@haverford.edu>
2016-05-25 19:36:46 -0700

    setup better choices for warnings in calculate descriptors

--- DRP/management/commands/calculate_descriptors.py
+++ DRP/management/commands/calculate_descriptors.py
@@ -7,8 +7,13 @@ class Command(BaseCommand):
     help = 'Recalculate the descriptors for all compounds and reactions.'
 
     def add_arguments(self, parser):
-        parser.add_argument('-e', '--error', action='store_true',
-                            help='Make warnings errors instead')
+        parser.add_argument('-e', '--error-level', nargs='?', default=0, const=3, type=int,
+                            help='Make warnings errors instead. '
+                                 '0 leaves python default settings '
+                                 '(or whatever settings are specified by the command line flags when calling python). '
+                                 '1 makes only RuntimeWarnings errors. '
+                                 '2 makes Runtime and User Warnings errors. '
+                                 '3 makes all warnings errors.')
         parser.add_argument('start', type=int, default=0, nargs='?',
                             help='pk of starting point. Indicates compound pk unless --reactions is specified')
         group = parser.add_mutually_exclusive_group(required=False)
@@ -22,9 +27,15 @@ class Command(BaseCommand):
         only_reactions = kwargs['reactions']
         only_compounds = kwargs['compounds']
         start = kwargs['start']
-        
-        if kwargs['error']:
+
+        if kwargs['error_level'] == 1:
+            warnings.simplefilter('error', RuntimeWarning)
+        if kwargs['error_level'] == 2:
+            warnings.simplefilter('error', UserWarning)
+            warnings.simplefilter('error', RuntimeWarning)
+        if kwargs['error_level'] == 3:
             warnings.simplefilter('error')
+
         if not only_reactions:
             Compound.objects.order_by('pk').filter(pk__gte=start).calculate_descriptors(verbose=verbose)
         if not only_compounds:
