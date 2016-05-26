351d8dc25730dbcc9c2bebe8588968769a351420
Philip Adler <padler1@haverford.edu>
2016-05-26 14:44:38 -0400

    fixed migration issues, and added limitations to the moles not being 0

---
+++ DRP/migrations/0026_auto_20160526_1429.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import DRP.models.validators
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('DRP', '0025_auto_20160524_1613'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='performedreaction',
+            name='performedDateTime',
+            field=models.DateTimeField(default=None, validators=[DRP.models.validators.notInTheFuture], blank=True, help_text=b'Timezone assumed EST, Date in format YYYY-MM-DD', null=True, verbose_name=b'Date Reaction Performed'),
+        ),
+    ]
