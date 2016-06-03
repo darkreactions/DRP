13532689f8d6a94a3c4405da9594e703334743f0
Philip Adler <padler1@haverford.edu>
2016-06-03 14:08:53 -0400

    hotfix for problems introduced by some server confusion

--- DRP/models/descriptors.py
+++ DRP/models/descriptors.py
@@ -56,7 +56,7 @@ class Descriptor(models.Model):
         ]
     )
     """A short label which is given to a description."""
-    name = models.CharField('Full name', max_length=255, unique=True)
+    name = models.CharField('Full name', max_length=300)
     calculatorSoftware = models.CharField(
         max_length=100,
         blank=True,
