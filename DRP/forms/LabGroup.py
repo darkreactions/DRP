813f04037ddb37e571487280d6ef358f34bd6336
Philip Adler <padler1@haverford.edu>
2016-06-03 10:46:49 -0400

    fixed bug with access code validation

--- DRP/forms/LabGroup.py
+++ DRP/forms/LabGroup.py
@@ -50,9 +50,9 @@ class LabGroupJoiningForm(forms.Form):
 
   def clean(self):
     super(LabGroupJoiningForm, self).clean()
-    if check_password(self.cleaned_data['accessCode'], self.cleaned_data['labGroup'].access_code):
+    if check_password(self.cleaned_data.get('accessCode'), self.cleaned_data['labGroup'].access_code):
       return self.cleaned_data
-    elif self.cleaned_data['accessCode'] == self.cleaned_data['labGroup'].legacy_access_code:
+    elif self.cleaned_data.get('accessCode') == self.cleaned_data['labGroup'].legacy_access_code:
       return self.cleaned_data
     else:
       raise ValidationError('Invalid Access Code', code='invalid_access')
