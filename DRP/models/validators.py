292b329ca61d4847d0f1b3a350b0b0860e6a26ee
Geoffrey Martin-Noble <gmartinn@haverford.edu>
2016-06-03 23:35:53 -0700

    fixed error in custom greater than validator string formatting

--- DRP/models/validators.py
+++ DRP/models/validators.py
@@ -11,7 +11,7 @@ class GreaterThanValidator(object):
 
     def __call__(self, value):
         if value <= self.floorValue:
-            raise ValidationError('This value must be greater than %(value)', params={'value':self.floorValue})
+            raise ValidationError('This value must be greater than %(value)s', params={'value':self.floorValue})
 
     def __eq__(self, other):
         return self.floorValue == other.floorValue
