2b9e97f099b9fe7ed0c3789e3ac3abc29f51f0c7
gmartinn <gmartinn@haverford.edu>
2016-06-05 13:50:14 -0400

    updated compare descriptors script

--- DRP/research/geoffrey/compare_descriptors.py
+++ DRP/research/geoffrey/compare_descriptors.py
@@ -26,7 +26,7 @@ def getDescValueType(desc_heading):
         return CatRxnDescriptorValue
     except CatRxnDescriptor.DoesNotExist:
         pass
-    raise ValueError('Invalid heading given. Does not match any descriptor')
+    raise ValueError('Invalid heading {}. Does not match any descriptor'.format(desc_heading))
 
 
 def compare(d1_heading, d2_heading, comparison_function=None, desc1ValueType=BoolRxnDescriptorValue, desc2ValueType=BoolRxnDescriptorValue, reactions=None):
@@ -37,26 +37,38 @@ def compare(d1_heading, d2_heading, comparison_function=None, desc1ValueType=Boo
     if reactions is None:
         reactions = PerformedReaction.objects.all()
 
-    d1_values = desc1ValueType.objects.filter(descriptor__heading=d1_heading)
-    d2_values = desc2ValueType.objects.filter(descriptor__heading=d2_heading)
+    d1_values = desc1ValueType.objects.filter(descriptor__heading=d1_heading, reaction__in=reactions)
+    d2_values = desc2ValueType.objects.filter(descriptor__heading=d2_heading, reaction__in=reactions)
     print "found {} values for descriptor 1, {} for descriptor 2".format(d1_values.count(), d2_values.count())
 
     print d1_heading, d2_heading
     different = []
 
-    count = 0
-    for d1_val in d1_values:
-        rxn = d1_val.reaction
-        if reactions.filter(id=rxn.id).exists():
-            count += 1
-            d2_val = d2_values.get(reaction=rxn)
-            if not comparison_function(d1_val.value, d2_val.value):
-                if abs(d1_val.value-d2_val.value) != 1 or water not in rxn.compounds.all():
-                    print rxn, d1_val.value, d2_val.value
-                    print rxn.compounds.all()
+    if d1_values.count() > d2_values.count():
+        for d1_val in d1_values:
+            rxn = d1_val.reaction
+            try:
+                d2_val = d2_values.get(reaction=rxn)
+            except BoolRxnDescriptorValue.DoesNotExist:
+                print rxn.performedreaction, d1_val.value, 'DoesNotExist', rxn.compounds.all()
                 different.append(rxn)
+            else:
+                if not comparison_function(d1_val.value, d2_val.value):
+                    print rxn.performedreaction, d1_val.value, d2_val.value, rxn.compounds.all()
+                    different.append(rxn)
+    else:
+        for d2_val in d2_values:
+            rxn = d2_val.reaction
+            try:
+                d1_val = d1_values.get(reaction=rxn)
+            except BoolRxnDescriptorValue.DoesNotExist:
+                print rxn.performedreaction, d1_val.value, 'DoesNotExist', rxn.compounds.all()
+                different.append(rxn)
+            else:
+                if not comparison_function(d1_val.value, d2_val.value):
+                    print rxn.performedreaction, d1_val.value, d2_val.value, rxn.compounds.all()
+                    different.append(rxn)
 
-    print "compared values for {} reactions".format(count)
     return different
 
 def get_references(reactions):
@@ -88,14 +100,15 @@ if __name__ == '__main__':
     tol_point_5 = shift_function(0, 0.55)
     #element = argv[1]
     water = Compound.objects.get(abbrev__iexact='H2O')
-
-    d2_heading = 'boolean_valence_1'
-    d1_heading = 'V0_legacy'
-    reactions = DataSet.objects.get(name='valid_legacy_rxns_nonzero_compound').reactions.all()
-    comparison_function = None
-    desc1ValueType = getDescValueType(d1_heading)
-    desc2ValueType = getDescValueType(d2_heading)
-    different = compare(d1_heading, d2_heading, desc1ValueType=desc1ValueType, desc2ValueType=desc2ValueType, comparison_function=comparison_function, reactions=reactions)
-    references = get_references(different)
-    print len(references)
-    print references
+    
+    for valence_num in range(0,8):
+        d2_heading = 'Inorg_drpInorgAtom_boolean_valence_{}_DRP_1.5_any'.format(valence_num)
+        d1_heading = 'V{}_legacy'.format(valence_num)
+        reactions = DataSet.objects.get(name='valid_legacy_rxns_nonzero_compound').reactions.all()
+        comparison_function = None
+        desc1ValueType = getDescValueType(d1_heading)
+        desc2ValueType = getDescValueType(d2_heading)
+        different = compare(d1_heading, d2_heading, desc1ValueType=desc1ValueType, desc2ValueType=desc2ValueType, comparison_function=comparison_function, reactions=reactions)
+        references = get_references(different)
+        print len(references)
+        print references
