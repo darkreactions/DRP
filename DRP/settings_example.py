aa2b455c1ca042ba1cd55c3f10f92aee3a96d631
Philip Adler <padler1@haverford.edu>
2016-06-20 10:44:14 -0400

    Added middleware to handle something going wrong with chemspider in a more informative manner.

--- DRP/settings_example.py
+++ DRP/settings_example.py
@@ -196,6 +196,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'DRP.middleware.ChemspiderErrorMiddleware'
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
