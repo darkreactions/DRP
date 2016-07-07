08f59811eceaf31a803241ca2b5ff2e5a74b507f
rlphilli <rlphillips@haverford.edu>
2016-07-07 14:11:13 -0400

    Adapted changes to drp.py imports

---
+++ DRP/management/commands/base_recommend.py
@@ -0,0 +1,28 @@
+import os
+
+import django
+
+import DRP.recommender as rec
+
+import os
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'DRP.settings'
+
+django.setup()
+
+
+from DRP.models.performedReaction import PerformedReaction
+
+
+from DRP.plugins.rxndescriptors.rxnhash import calculate_many
+
+calculate_many(PerformedReaction.objects.all())
+
+
+
+
+
+
+if __name__ == '__main__':
+    print('hey')
+    pass
---
+++ DRP/recommender/__init__.py
@@ -0,0 +1,4 @@
+from DRP.recommender.ReactionSieve import ReactionSieve
+from DRP.recommender.ReactionGenerator import ReactionGenerator
+from DRP.recommender.ReactionRecommender import Recommender
+
