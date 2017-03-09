0e404d2346a63ddf8cf89700041bf4539764907e
h205c <h205c@musk.cs.haverford.edu>
2017-03-09 05:25:35 -0500

    Re-removed extra argument to delete code that is currently broken in the master branch.

--- DRP/plugins/moldescriptors/chemaxon.py
+++ DRP/plugins/moldescriptors/chemaxon.py
@@ -319,7 +319,7 @@ def calculate(compound, verbose=False, whitelist=None):
                           v in descriptorDict.items() if k in whitelist}
     if verbose:
         logger.info("Deleting old descriptor values.")
-    delete_descriptors([compound], descriptorDict, cxcalcCommands)
+    delete_descriptors([compound], descriptorDict)
     filtered_cxcalcCommands = {
         k: v for k, v in cxcalcCommands.items() if k in descriptorDict.keys()}
     if verbose:
