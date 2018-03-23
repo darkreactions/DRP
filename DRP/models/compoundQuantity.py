6b35f7f8b8dd91cb22d19b1a2b3e4e71ab6e0783
yli11 <dylanzslack@gmail.com>
2018-03-23 17:06:03 -0400

    Rewriting old code

--- DRP/models/compoundQuantity.py
+++ DRP/models/compoundQuantity.py
@@ -52,6 +52,7 @@ class CompoundQuantity(models.Model):
     amount = models.DecimalField(null=True, blank=True, max_digits=12, decimal_places=5)
 
     def save(self, invalidate_models=True, *args, **kwargs):
+        self.amount = (self.amount_grams / self.compound.getMolecularWeight()) * 1000
         """Re-save associated reactions dependent upon this quantity as this will cause descriptor values to change."""
         super(CompoundQuantity, self).save(*args, **kwargs)
         try:
