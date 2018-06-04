9eaf40f9f568a55078f8d583fecbd3cc43737ec9
Dylan Emery <demery@haverford.edu>
2018-06-04 14:18:27 -0400

    added line chart, deleted some css

--- DRP/views/dashboardView.py
+++ DRP/views/dashboardView.py
@@ -3,8 +3,10 @@ from django.shortcuts import render, redirect
 from django.template import RequestContext, Context
 from DRP.models import PerformedReaction, LabGroup, ModelContainer
 from operator import add
+import datetime
 
-js_testing = False
+# Set to False for faster, non-dynamic page loads (good for testing JS)
+generate_csvs = True
 
 
 def dashboard(request):
@@ -13,7 +15,7 @@ def dashboard(request):
     num_experiments = len(PerformedReaction.objects.all())
     num_experiments_public = len(PerformedReaction.objects.filter(public=True))
     num_experiments_private = num_experiments - num_experiments_public
-    if not js_testing:
+    if generate_csvs:
         # get the dates the experiments were INSERTED into the datebase
         make_dates_csv('dateEntered.csv', 'inserted')
 
@@ -21,7 +23,12 @@ def dashboard(request):
         make_dates_csv('datePerformed.csv', 'performed')
 
         # get the date the experiments were PERFORMED, count the num experiments cumulatively
-        make_dates_csv('datePerformedCumulative.csv', 'performed', cumulative=True)
+        make_dates_csv('datePerformedCumulativeByLab.csv', 'performed', cumulative=True)
+
+        # get the date the experiments were PERFORMED, count the num experiments cumulatively, not by lab though
+        today = datetime.datetime.today()
+        timeframe = datetime.timedelta(days=30)
+        make_dates_csv_no_lab('datePerformedCumulative.csv', 'performed', cumulative=True, add_inbetween_times=True, dateRange=(today - timeframe, today))
 
     # Another thing to add would be the confusion matrices.
     # Unfortunately, there is no data for these in the test dataset I have, so this just prints an empty list
@@ -61,11 +68,12 @@ def make_dates_csv(csv_name, inserted_or_performed, cumulative=False):
             elif inserted_or_performed == "performed":
                 date = str(reaction.performedDateTime)[:10]
             else:
-                raise ValueError("Arguement 'inserted_or_performed' must be 'inserted' or 'performed'")
+                raise ValueError("Argument 'inserted_or_performed' must be 'inserted' or 'performed'")
 
             # many dates are 'None', I am just skipping those
             if date == 'None':
                 continue
+
             user = reaction.user
             labGroups = []
             if user:
@@ -97,3 +105,57 @@ def make_dates_csv(csv_name, inserted_or_performed, cumulative=False):
             for date in date_dictionary:
                 lab_group_counts = map(str, date_dictionary[date])
                 f.write(date + "," + ",".join(lab_group_counts) + "\n")
+
+
+def make_dates_csv_no_lab(csv_name, inserted_or_performed, cumulative=False, add_inbetween_times=False, dateRange=None):
+    """Generate a dates csv without accounting for the different labs.
+
+    Used the create the line graph next to the number of experiments.
+    Not tested throughly, but tested.
+    """
+    with open('static/' + csv_name, 'w') as f:
+        date_dictionary = {}
+        if add_inbetween_times:
+            if dateRange:
+                d1 = dateRange[0]
+                d2 = dateRange[1]
+            else:
+                d1 = datetime.date(2015, 7, 8)
+                d2 = datetime.date(2018, 1, 21)
+            delta = d2 - d1
+
+            for i in range(delta.days + 1):
+                date_dictionary[str(d1 + datetime.timedelta(i))[:10]] = 0
+        for reaction in PerformedReaction.objects.all():
+            if inserted_or_performed == "inserted":
+                date = reaction.insertedDateTime
+            elif inserted_or_performed == "performed":
+                date = reaction.performedDateTime
+            else:
+                raise ValueError("Argument 'inserted_or_performed' must be 'inserted' or 'performed'")
+
+            if date is None or (dateRange and (date < dateRange[0] or date > dateRange[1])):
+                continue
+
+            date = str(date)[:10]
+            if date not in date_dictionary:
+                date_dictionary[date] = 0
+            date_dictionary[date] += 1
+
+        # write the headers
+        f.write("date,count\n")
+        if cumulative:
+            previous = 0
+            ordered_dates = []
+            # write the dates and corresponding entry counts
+            for date in date_dictionary:
+                ordered_dates.append((date, date_dictionary[date]))
+            ordered_dates.sort()
+            for date in ordered_dates:
+                cumulative_count = date[1] + previous
+                previous = cumulative_count
+                f.write(date[0] + "," + str(cumulative_count) + "\n")
+
+        else:
+            for date in date_dictionary:
+                f.write(date + "," + str(date_dictionary[date]) + "\n")
