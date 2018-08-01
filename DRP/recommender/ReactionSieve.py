bd127f3a952cfd8927f481f6dc3594e6e188b4ce
csvillalta <christophervillalta@gmail.com>
2018-08-01 15:49:41 -0400

    cleanup

--- DRP/recommender/ReactionSieve.py
+++ DRP/recommender/ReactionSieve.py
@@ -11,9 +11,9 @@ class ReactionSieve(object):
         if verbose:
             print('''ENTERING FILTER''')
 
-        a = self.model_container.predict(reactions)  # , verbose=True)
+        predictions = self.model_container.predict(reactions)  # , verbose=True)
         print("--------")
-        print(a)
+        print(predictions)
         print("--------")
         if verbose:
             print('''PREDICTIONS DONE''')
@@ -22,27 +22,11 @@ class ReactionSieve(object):
         if verbose:
             print('''DESIRED DESCRIPTORS:''')
             print((self.desired_desc_dict))
-        # reactions_from_dict = list(a.items())[0][1]
-        # for tuple in reactions_from_dict:
-        #     plausible_reactions.append(tuple[0])
-
-        # # NOT SURE WHAT THIS DOES
-        # # for desc, desc_vals in list(self.desired_desc_dict.items()):
-        # #     for i in desc_vals:
-        # #         for a in a[i]:
-        # #             plausible_reactions.append(a[0])
-        # plausible_reactions_ids = [pr.id for pr in plausible_reactions]
-        # reactions = reactions.filter(pk__in=plausible_reactions_ids)
 
         for descriptor in self.desired_desc_dict:
-            for prediction in a[descriptor]:
+            for prediction in predictions[descriptor]:
                 if prediction[1].value in self.desired_desc_dict[descriptor]:
                     plausible_reactions[prediction[0].id] = prediction[1].value
-        # plausible_reactions_ids = [pr.id for pr in plausible_reactions]
-        # reactions = reactions.filter(pk__in=plausible_reactions_ids)
-
-        
-
         if verbose:
             print('''EXITING FILTER''')
         return plausible_reactions
